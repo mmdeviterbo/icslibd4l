fb1902fc4a9fd59fd59c3644ee468b539bd4668c
Bien Custodio <bbcustodio2>
2021-04-30 16:37:23 +0800

    EDIT: added database credentials

--- server/index.js
+++ server/index.js
@@ -4,12 +4,15 @@ const config = require("config")
 
 const app = express();
 const PORT = config.get('port');
-
+//database USER:
+//username: sampleuser
+//password: password1234
 //implements code in startup/db.js
 require('./startup/db')();
-
+require('./startup/routes');
 //starts listening in PORT 3001
 app.listen(PORT, () => {
     console.log(`Listening to Port ${PORT}`);
 });
+
 
