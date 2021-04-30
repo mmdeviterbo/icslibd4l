18b063e7857be6233d58bd5650665a03bd313592
Bien Custodio <bbcustodio2>
2021-04-30 14:30:43 +0800

    ADD: initial state of project in sprint 1

--- server/index.js
+++ server/index.js
@@ -1,1 +1,15 @@
 //starting file of the backend
+const express = require('express');
+const config = require("config")
+
+const app = express();
+const PORT = config.get('port');
+
+//implements code in startup/db.js
+require('./startup/db')();
+
+//starts listening in PORT 3001
+app.listen(PORT, () => {
+    console.log(`Listening to Port ${PORT}`);
+});
+
