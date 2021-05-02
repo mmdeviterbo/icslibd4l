b5cd016210a4099cac7ff81e9107a2594c6ec47e
karlbrazil <krbrazil@up.edu.ph>
2021-05-02 18:55:10 +0800

    Edit: index.js

--- server/index.js
+++ server/index.js
@@ -8,9 +8,6 @@ const PORT = config.get('port');
 
 dotenv.config();
 
-//database USER:
-//username: sampleuser
-//password: password1234
 //implements code in startup/db.js
 require('./startup/db')();
 require('./startup/routes');
