3b88319e83cfa1a8bb799379e208b7d62b422d80
ryanresoles <rbresoles@up.edu.ph>
2021-05-02 21:00:50 +0800

    sprint 1 - resoles ICS model

--- server/index.js
+++ server/index.js
@@ -1,1 +1,55 @@
 //starting file of the backend
+const express = require('express');
+const config = require("config");
+const crypto = require("crypto");
+const app = express();
+
+app.use(express.json());
+
+//user file from the models directory
+const ics = require('./models/ICS');
+
+
+app.post('/add', async (req, res) => {
+    // encryption of password
+    // let iv = crypto.randomBytes(16);
+    // let key = crypto.createDecipheriv('aes-128-cbc', Buffer.from('userpassword', 'hex'), iv);
+    // let encryptedPass = key.update('pass1234');
+    // encryptedPass = Buffer.concat([encryptedPass, key.final()])
+    const icsAdmins = new ics({ adminName: "Ryan Resoles", adminPassword: 'pass1234', adminEmail: "sample@up.edu.ph"});
+
+    try{
+        await icsAdmins.save();
+        res.send('User Added.')
+    } catch(err) {
+        console.log(err)
+    }
+});
+
+app.get('/retrieve', async (req, res) => {
+    ics.find({ adminName: "Ryan Resoles"}, (err, result) => {
+        if(err){
+            res.send(err);
+        }
+
+        res.send(result);
+    })
+
+})
+
+
+
+
+const PORT = config.get('port');
+//database USER:
+//username: sampleuser
+//password: password1234
+//implements code in startup/db.js
+require('./startup/db')();
+require('./startup/routes');
+//starts listening in PORT 3001
+app.listen(PORT, () => {
+    console.log(`Listening to Port ${PORT}`);
+});
+
+
