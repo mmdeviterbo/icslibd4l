60bbc3aea0e322b9f65f81ae749c673d9ae4057e
karlbrazil <krbrazil@up.edu.ph>
2021-05-02 18:18:13 +0800

    Add: bookRouter.js and thesisRouter.js

--- server/models/booksModel.js
+++ server/models/booksModel.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 
 const bookSchema = new mongoose.Schema({
     title: {type:String, required: true},
-    author: {type:String, required: true},
-    edition: {type:Number, required:true}
+    author: {type:String, required: true}
 });
 
 const BookModel = mongoose.model("book", bookSchema);
---
+++ server/routes/thesisRouter.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+const thesisModel = require("../models/thesisModel");
+
+router.post("/", async (req,res)=>{
+    try{
+        const {title, author, year} = req.body; 
+
+        // sample verification: incomplete fields
+        if(!title||!author||!year){
+            return res.status(400).json({errorMessage:"Please enter all required fields."});
+        };
+    
+        // save to database
+        const newThesis = new thesisModel ({
+            title, author, year
+        });
+
+        const savedThesis = await newThesis.save();
+        
+    } catch(err){
+        console.log(err);
+        res.status(500).send();
+    }
+});
+
+module.exports = router;
