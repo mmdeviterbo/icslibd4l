7c001334e1f9237ab0eb02395ad76a09990611c0
Marty de Viterbo <mjennety@gmail.com>
2021-05-07 17:39:56 +0800

    Update service directory

--- src/services/personService.js
+++ src/services/personService.js
@@ -1,6 +1,7 @@
 import http from './httpService';
 import {apiEndpoint} from '../config.json';
 
+
 // login/register a person (guess, student, admin, faculty)
 const loginRegisterUser=()=>{
     return http.post(apiEndpoint+'/authentication/create');
