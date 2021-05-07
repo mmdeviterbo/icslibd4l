c3111bdeb07feafd78f4270e9743f016dcd79abe
Ryan Vincent Cabuslay <rlcabuslay1@up.edu.ph>
2021-05-07 20:20:45 +0800

    add: read and update users

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -43,9 +43,10 @@ router.post("/create", async (req,res) => {
             loggedUser = existingUser;
         }
         else{   
-            const userType = "student"
+            const userType = 4
+            const nickname = fullName
             const newUser = new UserModel ({
-                googleId, email, fullName, userType
+                googleId, email, fullName, userType, nickname
             });
     
             //save new user entry in mongoDB
@@ -65,6 +66,43 @@ router.post("/create", async (req,res) => {
         res.cookie("token", token, {
             httpOnly: true,
         }).send(loggedUser);
+    }
+    catch (err){
+        console.error(err)
+        res.status(500).send();
+    }
+});
+
+//read
+router.get("/read", async (req, res) => {
+    UserModel.find({}, (err, result) => { //reads all the documents and sends as response
+        if (err) {
+            res.send(err);
+        } else {
+        res.send(result);
+        }
+    });
+});
+
+//update
+router.put("/update", async (req, res) => {
+    try{
+        const {googleId, newNickname} = req.body; //get googleId and newNickname from body
+
+        if(newNickname){ //update nickname field
+            await UserModel.updateOne({googleId: googleId}, {nickname:newNickname}, {
+                new: true
+            });
+        }
+
+        UserModel.find({googleId: googleId}, (err, result) => { //send the edited user as response
+            if (err) {
+                res.send(err);
+            } else {
+            res.send(result);
+            }
+        });
+
     }
     catch (err){
         console.error(err)
