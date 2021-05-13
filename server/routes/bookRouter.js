fb898f9c6b966d65759c9f94ca067a8379d7f48c
Ryan Vincent Cabuslay <rlcabuslay1@up.edu.ph>
2021-05-13 20:42:15 +0800

    docs: add comments

--- server/routes/bookRouter.js
+++ server/routes/bookRouter.js
@@ -21,23 +21,26 @@ router.post("/create", authFaculty, async (req,res)=>{
         //search if book exists
         const existingBook = await bookModel.findOne({ bookId });
     
-        if(!existingBook){
-            const newBook = new bookModel ({
+        if(!existingBook){ //if book does not exist, add the book
+
+            const newBook = new bookModel ({ //add the non-array fields to the books collection
                 bookId, title, physicalDesc, publisher, numberOfCopies
             });
             const savedBook = await newBook.save();
             
-            authors.forEach(async function(entry) {
+            authors.forEach(async function(entry) { //add each author to the book_authors collection
                 const author_fname = entry.fname;
                 const author_lname = entry.lname;
+
                 const newBookAuthor = new bookAuthorModel ({
                     bookId, author_fname, author_lname
                 });
                 const savedBookAuthor = await newBookAuthor.save();
             });
 
-            subjects.forEach(async function(entry) {
+            subjects.forEach(async function(entry) { //add each subject to the book_subjects collection
                 const subject = entry;
+
                 const newBookSubject = new bookSubjectModel ({
                     bookId, subject
                 });
@@ -46,8 +49,8 @@ router.post("/create", authFaculty, async (req,res)=>{
             
             res.json(savedBook);
         }
-        else{
-            res.send("Book already exists!");
+        else{ //sends a 400 status if book already exists
+            res.status(400).send("Book already exists!");
         }
         
     } catch(err){
