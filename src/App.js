d9ac6c4d2ed9be5762d76592a645e46e5526b962
Marty de Viterbo <mjennety@gmail.com>
2021-05-13 22:34:59 +0800

    Update css design for parallal efect, homepage mobile responsiveness

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ function App() {
 
 
 
-          {/* <Route path="/about" component={About}/> */}
+          <Route path="/about" render={()=><About appRef={appRef}/>}/>
           <Route exact path="/not-found" component={Notfound}></Route> 
           <Redirect exact from="/" to="/home"/>
           <Redirect to="/not-found"/> 
