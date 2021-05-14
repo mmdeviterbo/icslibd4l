8ab5839962a548231caa617c0478a2fc4c552fa9
Bien Custodio <bbcustodio2>
2021-05-14 19:58:06 +0800

    EDIT: cleaned code

--- server/middleware/authStudent.js
+++ server/middleware/authStudent.js
@@ -8,7 +8,6 @@ const jwtPrivateKey = config.get('jwtPrivateKey');
 function authenticationStudent(req, res, next){
     try{
         const token = req.cookies.token;
-        console.log(req.cookies);
         //checks if token exists
         if (!token)
             return res
