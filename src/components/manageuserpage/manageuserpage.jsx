fa57383a4895e10fa8732d6e8b16ab966bd05384
johnmelr <jrramos7@up.edu.ph>
2021-05-17 21:13:26 +0800

    ManageUserPage takes in the current user logged in as a parameter and passes it on to the userTable component

--- src/components/manageuserpage/manageuserpage.jsx
+++ src/components/manageuserpage/manageuserpage.jsx
@@ -3,15 +3,14 @@ import FilterMenu from './filterMenu'
 import UserTable from './userTable'
 import UserSearch from './userSearch'
 
-export default function ManageUserPage() {
-
+export default function ManageUserPage({user}) {
 
   return (
     <div className="manageuser-container">
       <UserSearch />
       <FilterMenu />
       <div className="usertable-container" style={{padding: '50px', backgroundColor: '#F5F5F5'}}>
-        <UserTable />
+        <UserTable user={user}/>
       </div>
     </div>
   )
