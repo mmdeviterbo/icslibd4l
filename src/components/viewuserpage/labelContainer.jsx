fa8bb09fc40a0a71db9387f34db3bff1c319b181
Elcid Cruzado <escruzado@up.edu.ph>
2021-05-18 02:45:44 +0800

    Fixed saved state

--- src/components/viewuserpage/labelContainer.jsx
+++ src/components/viewuserpage/labelContainer.jsx
@@ -7,107 +7,112 @@ import { faCheck, faPencilAlt } from "@fortawesome/free-solid-svg-icons";
 
 // component that returns an edit button
 const EditNickname = () => {
-  const [click, setClick] = useState(false);
-  const [style, setStyle] = useState(editButtonDefault);
-  const [buttonStyle, setButtonStyle] = useState(faPencilAlt);
-  const [disable, setDisable] = useState(false);
+    const [click, setClick] = useState(false);
+    const [style, setStyle] = useState(editButtonDefault);
+    const [buttonStyle, setButtonStyle] = useState(faPencilAlt);
+    const [disable, setDisable] = useState(false);
 
-  const setIcon = (click, buttonStyle, style) => {
-    setClick(click);
-    setButtonStyle(buttonStyle);
-    setStyle(style);
-    setDisable(!disable);
-  };
+    const setIcon = (click, buttonStyle, style) => {
+        setClick(click);
+        setButtonStyle(buttonStyle);
+        setStyle(style);
+        setDisable(!disable);
+    };
 
-  return (
-    <FontAwesomeIcon
-      onClick={() =>
-        click === false
-          ? setIcon(true, faCheck, editButtonConfirm)
-          : setIcon(false, faPencilAlt, editButtonDefault)
-      }
-      state={click}
-      aria-label="edit"
-      style={style}
-      icon={buttonStyle}
-    />
-  );
+    return (
+        <FontAwesomeIcon
+            onClick={() =>
+                click === false
+                    ? setIcon(true, faCheck, editButtonConfirm)
+                    : setIcon(false, faPencilAlt, editButtonDefault)
+            }
+            state={click}
+            aria-label="edit"
+            style={style}
+            icon={buttonStyle}
+        />
+    );
 };
 
 export default function LabelContainer({
-  msg,
-  userinfos,
-  isType,
-  iseditable,
-  disable,
+    msg,
+    userinfos,
+    isType,
+    iseditable,
+    disable,
 }) {
-  const [type, setType] = useState(userinfos);
-  const [hasButton, setHasButton] = useState();
+    const [type, setType] = useState(userinfos);
+    const [hasButton, setHasButton] = useState();
 
-  const setButton = (btn) => {
-    setHasButton(btn);
-  };
+    const setButton = (btn) => {
+        setHasButton(btn);
+    };
 
-  useEffect(() => {
-    if (isType) {
-      if (userinfos === 1) setType("Admin");
-      else if (userinfos === 2) setType("Faculty");
-      else if (userinfos === 3) setType("Staff");
-      else setType("Student");
-    }
-    iseditable ? setButton(<EditNickname />) : setButton();
-  }, []);
+    useEffect(() => {
+        console.log(userinfos);
+        if (isType) {
+            if (userinfos === 1) setType("Admin");
+            else if (userinfos === 2) setType("Faculty");
+            else if (userinfos === 3) setType("Staff");
+            else setType("Student");
+        }
 
-  return (
-    <Container fluid>
-      <Row>
-        <Col xs={4} className="columns-temp">
-          <div className="labelText" style={labelText}>
-            {msg}
-          </div>
-        </Col>
-        <Col xs={7} className="columns-temp">
-          <TextField
-            // InputLabelProps={{style: {fontSize: 20}}}
-            variant="outlined"
-            fullWidth="true"
-            size="medium"
-            disabled="true"
-            value={type}
-            inputProps={{ style: { fontSize: 20 } }} // font size of input text
-            style={infoTextField}></TextField>
-        </Col>
-        <Col xs={1} className="edit-column">
-          {hasButton}
-        </Col>
-      </Row>
-    </Container>
-  );
+        // IF TYPE === NICKNAME
+        //     VALUE  = USER.NICKNAME
+
+        iseditable ? setButton(<EditNickname />) : setButton();
+    }, []);
+
+    return (
+        <Container fluid>
+            <Row>
+                <Col xs={4} className="columns-temp">
+                    <div className="labelText" style={labelText}>
+                        {msg}
+                    </div>
+                </Col>
+                <Col xs={7} className="columns-temp">
+                    <TextField
+                        // InputLabelProps={{style: {fontSize: 20}}}
+                        variant="outlined"
+                        fullWidth="true"
+                        size="medium"
+                        disabled="true"
+                        value={type}
+                        inputProps={{ style: { fontSize: 20 } }} // font size of input text
+                        style={infoTextField}></TextField>
+                </Col>
+                <Col xs={1} className="edit-column">
+                    {hasButton}
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
 const infoTextField = {
-  fontSize: "15px",
-  // background: "black",
-  // border: "white",
+    fontSize: "15px",
+    // background: "black",
+    // border: "white",
 };
 
 const labelText = {
-  verticalAlign: "center",
-  paddingTop: "22px",
-  fontWeight: "900",
-  fontSize: "25px",
+    verticalAlign: "center",
+    paddingTop: "22px",
+    fontWeight: "900",
+    fontSize: "25px",
 };
 
 const editButtonDefault = {
-  color: "gray",
-  margin: "12px 0 0px -10px",
-  width: "20px",
-  height: "20px",
+    color: "gray",
+    margin: "12px 0 0px -10px",
+    width: "20px",
+    height: "20px",
 };
 
 const editButtonConfirm = {
-  color: "#90ee90",
-  margin: "12px 0 0px -10px",
-  width: "20px",
-  height: "20px",
+    color: "#90ee90",
+    margin: "12px 0 0px -10px",
+    width: "20px",
+    height: "20px",
 };
--- src/components/viewuserpage/profileContainer.jsx
+++ src/components/viewuserpage/profileContainer.jsx
@@ -15,122 +15,189 @@ import LabelContainer from "./labelContainer";
 import "../../styles/userPageStyle.css";
 
 export default function ProfileContainer({ user }) {
-  useEffect(() => {
-    console.log(user);
-  }, []);
-  // removes JWT token from the browser
-  const logout = async () => {
-    try {
-      localStorage.removeItem(jwtPrivateKey);
-      await PersonService.logoutUser(user);
-      window.location = "/";
-    } catch (err) {}
-  };
+    useEffect(() => {
+        const nickname = {};
+        console.log(user);
+    }, []);
 
-  return (
-    // column for the title bar "Profile Display"
-    <Container fixed className="profile-container">
-      <Row className="title-bar">
-        <Col xs={2}></Col>
-        <Col xs={8}>
-          <div className="headerText" style={headerText}>
-            Profile Display
-          </div>
-        </Col>
-        <Col xs={1} className="columns-temp"></Col>
-      </Row>
-      <Row>
-        <Col xs={2}></Col>
-        <Col xs={8} className="columns-temp">
-          <LabelContainer
-            msg={"Nickname: "}
-            userinfos={user.nickname}
-            iseditable={true}
-          />
-        </Col>
-        <Col xs={1} className="columns-temp"></Col>
-      </Row>
-      <Row>
-        <Col xs={2}></Col>
-        <Col xs={8} className="columns-temp">
-          <LabelContainer msg={"Name: "} userinfos={user.fullName} />
-        </Col>
-        <Col xs={1} className="columns-temp"></Col>
-      </Row>
-      <Row>
-        <Col xs={2}></Col>
-        <Col xs={8} className="columns-temp">
-          <LabelContainer
-            msg={"Classification: "}
-            userinfos={user.userType}
-            isType={true}
-          />
-        </Col>
-        <Col xs={1} className="columns-temp"></Col>
-      </Row>
-      <Row>
-        <Col xs={2}></Col>
-        <Col xs={8} className="columns-temp">
-          <LabelContainer msg={"Email: "} userinfos={user.email} />
-        </Col>
-        <Col xs={1} className="columns-temp"></Col>
-      </Row>
-      {/* part for account removal */}
-      <Row className="removal-bar">
-        <Col xs={2}></Col>
-        <Col xs={8}>
-          <div
-            className="header-text"
-            style={{ fontWeight: "900", fontSize: "30px" }}>
-            Account Removal
-          </div>
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={2}></Col>
-        <Col xs={8} className="columns-temp">
-          <div style={{ padding: "12px", fontSize: "15px" }}>
-            Removing your accounts means dissociating your account from the app.
-          </div>
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={2}></Col>
-        <Col xs={8}>
-          {/* BACKLOG: 
-        - MODAL OR POP-UP TO WARN USER ABOUT DISSOCIATION
-        - ACCOUNT DISSOCIATION (FROM BACKEND(?)) */}
-          <Button
-            onClick={logout}
-            variant="contained"
-            color="secondary"
-            className="delete-button"
-            style={{ paddingLeft: "10px" }}
-            startIcon={<DeleteIcon />}
-            fontWeight="900">
-            Remove Account
-          </Button>
-        </Col>
-      </Row>
-    </Container>
-  );
+    // removes JWT token from the browser
+    const logout = async () => {
+        try {
+            localStorage.removeItem(jwtPrivateKey);
+            await PersonService.logoutUser(user);
+            window.location = "/";
+        } catch (err) {}
+    };
+
+    return (
+        // column for the title bar "Profile Display"
+        <div class="container" className="profile-container">
+            <div class="row" className="title-bar">
+                <div class="col" l className="header-text" style={headerText}>
+                    Profile Display
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-4" className="label-text" style={labelText}>
+                    Nickname:
+                </div>
+                <div class="col-4" className="text-field-container">
+                    <input
+                        className="text-field"
+                        value={user && user.nickname}
+                    />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-4" className="label-text" style={labelText}>
+                    Name:
+                </div>
+                <div class="col-4" className="text-field-container">
+                    <input
+                        className="text-field"
+                        value={user && user.fullName}
+                    />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-4" className="label-text" style={labelText}>
+                    Classification:
+                </div>
+                <div class="col-4" className="text-field-container">
+                    <input
+                        className="text-field"
+                        value={user && user.userType}
+                    />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-4" className="label-text" style={labelText}>
+                    Email:
+                </div>
+                <div class="col-4" className="text-field-container">
+                    <input className="text-field" value={user && user.email} />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col" className="label-text" style={labelText}></div>
+            </div>
+        </div>
+
+        // <Container fixed className="profile-container">
+        //   <Row className="title-bar">
+        //     <Col xs={2}></Col>
+        //     <Col xs={8}>
+        //       <div className="headerText" style={headerText}>
+        //         Profile Display
+        //       </div>
+        //     </Col>
+        //     <Col xs={1} className="columns-temp"></Col>
+        //   </Row>
+        //   <Row>
+        //     <Col xs={2}></Col>
+        //     <Col xs={8} className="columns-temp">
+        //       <input
+        //         value={user && user.nickname}
+        //       />
+        //       <LabelContainer
+        //         msg={"Nickname: "}
+        //         // access mo 'to sa database
+        //         userinfos={user && user.nickname}
+        //         // userinfos={user}
+        //         iseditable={true}
+        //         // whatInfo={"nickname"}
+        //       />
+        //     </Col>
+        //     <Col xs={1} className="columns-temp"></Col>
+        //   </Row>
+        //   <Row>
+        //     <Col xs={2}></Col>
+        //     <Col xs={8} className="columns-temp">
+        //       <LabelContainer msg={"Name: "} userinfos={user && user.fullName} />
+        //     </Col>
+        //     <Col xs={1} className="columns-temp"></Col>
+        //   </Row>
+        //   <Row>
+        //     <Col xs={2}></Col>
+        //     <Col xs={8} className="columns-temp">
+        //       <LabelContainer
+        //         msg={"Classification: "}
+        //         userinfos={user && user.userType}
+        //         isType={true}
+        //       />
+        //     </Col>
+        //     <Col xs={1} className="columns-temp"></Col>
+        //   </Row>
+        //   <Row>
+        //     <Col xs={2}></Col>
+        //     <Col xs={8} className="columns-temp">
+        //       <LabelContainer msg={"Email: "} userinfos={user && user.email} />
+        //     </Col>
+        //     <Col xs={1} className="columns-temp"></Col>
+        //   </Row>
+        //   {/* part for account removal */}
+        //   <Row className="removal-bar">
+        //     <Col xs={2}></Col>
+        //     <Col xs={8}>
+        //       <div
+        //         className="header-text"
+        //         style={{ fontWeight: "900", fontSize: "30px" }}>
+        //         Account Removal
+        //       </div>
+        //     </Col>
+        //   </Row>
+        //   <Row>
+        //     <Col xs={2}></Col>
+        //     <Col xs={8} className="columns-temp">
+        //       <div style={{ padding: "12px", fontSize: "15px" }}>
+        //         Removing your accounts means dissociating your account from the app.
+        //       </div>
+        //     </Col>
+        //   </Row>
+        //   <Row>
+        //     <Col xs={2}></Col>
+        //     <Col xs={8}>
+        //       {/* BACKLOG:
+        //     - MODAL OR POP-UP TO WARN USER ABOUT DISSOCIATION
+        //     - ACCOUNT DISSOCIATION (FROM BACKEND(?)) */}
+        //       <Button
+        //         onClick={logout}
+        //         variant="contained"
+        //         color="secondary"
+        //         className="delete-button"
+        //         style={{ paddingLeft: "10px" }}
+        //         startIcon={<DeleteIcon />}
+        //         fontWeight="900">
+        //         Remove Account
+        //       </Button>
+        //     </Col>
+        //   </Row>
+        // </Container>
+    );
 }
 
 const headerText = {
-  fontWeight: "900",
-  fontSize: "35px",
+    fontWeight: "900",
+    fontSize: "35px",
+};
+
+const labelText = {
+    verticalAlign: "center",
+    // paddingTop: "22px",
+    fontWeight: "900",
+    fontSize: "25px",
 };
 
 const editButtonDefault = {
-  color: "gray",
-  margin: "24px 0 0px -30px",
-  width: "20px",
-  height: "20px",
+    color: "gray",
+    margin: "24px 0 0px -30px",
+    width: "20px",
+    height: "20px",
 };
 
 const editButtonConfirm = {
-  color: "#90ee90",
-  margin: "24px 0 0px -30px",
-  width: "20px",
-  height: "20px",
+    color: "#90ee90",
+    margin: "24px 0 0px -30px",
+    width: "20px",
+    height: "20px",
 };
