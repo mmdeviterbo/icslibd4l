6f6a6138aa66874e8bba632508bced4207734af5
johncaleb <jccanlas3@up.edu.ph>
2021-05-20 03:36:39 +0800

    added search for users function

--- server/routes/adminRouter.js
+++ server/routes/adminRouter.js
@@ -60,44 +60,67 @@ router.put("/updateOtherUser", authAdmin, async (req, res) => {
 
 //search function
 router.get("/search", authAdmin, async (req, res) => {
-    let query;
+    let idList = [];
+    let init_output;
     let final_output;
-    let attributes = 0;
+
+    function saveId(item, index){
+        if(!idList){
+            idList = item._id;
+        }
+        else{
+            idList = [].concat(idList, item._id);
+        }
+    }
     
     if (req.query.search){
-        //seach quries for all attributes
-        while(attributes < 3){
-            query = {};
-            //fullName
-            if (attributes === 0){
-                query.fullName = {
+        //seach queries for email, name, and nickname attributes
+        //email
+        init_output = await UserModel.find({
+                email: {
                     $regex: req.query.search,
                     $options: 'i'
                 }
-            }
-            //email
-            else if (attributes == 1){
-                query.email = {
+            });
+        //add to final list
+        final_output = init_output;
+        console.log(final_output);
+        //add _id to idList
+        init_output.forEach(saveId);
+
+        //fullName
+        init_output = await UserModel.find({
+                fullName: {
                     $regex: req.query.search,
                     $options: 'i'
+                },
+
+                _id: {
+                    $nin: idList
                 }
-            }
-            //googleId
-            else if(attributes == 2){
-                query.googleId = {
+            });
+        //add to final list
+        final_output = [].concat(final_output, init_output);
+        console.log(final_output);
+        //add _id to idList
+        init_output.forEach(saveId);
+        console.log(idList);
+        
+        //nickName
+        init_output = await UserModel.find({
+                nickName: {
                     $regex: req.query.search,
                     $options: 'i'
+                },
+
+                _id: {
+                    $nin: idList
                 }
-            }
-            //query to database
-            let users = await UserModel.find(query);
-            //concatenate to final array of objcets
-            if (!final_output)
-                final_output = users;
-            else
-            final_output =[].concat(final_output,users);
-            attributes = attributes + 1;
-        }
+            });
+        //add to final list
+        final_output = [].concat(final_output, init_output);
+        //add _id to idList
+        init_output.forEach(saveId);
        
     }
     else{
