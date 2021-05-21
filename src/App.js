9485eac78c19272ac91df4dc444b15814b7f433f
Elcid Cruzado <escruzado@up.edu.ph>
2021-05-22 00:12:46 +0800

    Fix: user state management at account-settings

--- src/App.js
+++ src/App.js
@@ -1,116 +1,133 @@
-import {Route, Switch, Redirect, useParams } from 'react-router-dom';
-import {useState, useEffect, useRef} from 'react';
-import Footer from './components/footer';
-import Homepage from './components/homepage/homepage';
-import NavigationBar from './components/navigationBar';
-import Notfound from './components/notfound';
-import ManageResPage from './components/manageresourcespage/manageresourcespage'
-import AddBookPage from './components/addresourcepage/add-new-resource-pg'
-import AddSPThesisPage from './components/addresourcepage/add-spt-pg-container'
+import { Route, Switch, Redirect, useParams } from "react-router-dom";
+import { useState, useEffect, useRef } from "react";
+import Footer from "./components/footer";
+import Homepage from "./components/homepage/homepage";
+import NavigationBar from "./components/navigationBar";
+import Notfound from "./components/notfound";
+import ManageResPage from "./components/manageresourcespage/manageresourcespage";
+import AddBookPage from "./components/addresourcepage/add-new-resource-pg";
+import AddSPThesisPage from "./components/addresourcepage/add-spt-pg-container";
 import ManageUser from "./components/manageuserpage/manageuserpage";
 import ViewUserPage from "./components/viewuserpage/viewUserPage";
-import personService from './services/personService';
+import personService from "./services/personService";
 
-import {jwtPrivateKey} from './config.json';
-import {jwtEncryptionKey} from './config.json';
-import * as jwtEncrypt from 'jwt-token-encrypt';
-import './App.css';
-import AddResource from './components/crud/add';
-import ViewResource from './components/crud/view';
-import updateResourceData from './components/crud/update';
-import About from './components/about/about';
+import { jwtPrivateKey } from "./config.json";
+import { jwtEncryptionKey } from "./config.json";
+import * as jwtEncrypt from "jwt-token-encrypt";
+import "./App.css";
+import AddResource from "./components/crud/add";
+import ViewResource from "./components/crud/view";
+import updateResourceData from "./components/crud/update";
+import About from "./components/about/about";
 
 function App() {
-  const [user, setUser] = useState(null); //fullname, email, userType (integer)
+    const [user, setUser] = useState(null); //fullname, email, userType (integer)
 
-  const browseRef = useRef(null);
-  const latestAcqRef = useRef(null);
-  const newsRef = useRef(null);
-  const appRef = useRef(null);
+    const browseRef = useRef(null);
+    const latestAcqRef = useRef(null);
+    const newsRef = useRef(null);
+    const appRef = useRef(null);
 
-  useEffect(() => {
-    getCurrentToken();
-  }, []);
+    useEffect(() => {
+        getCurrentToken();
+    }, []);
 
-  // see if there's current user logged in the browser
-  const getCurrentToken = () => {
-    try {
-      const jwt = localStorage.getItem(jwtPrivateKey);
-      const encryption = {
-        key: jwtEncryptionKey,
-        algorithm: 'aes-256-cbc',
-      };
-      const decrypted = jwtEncrypt.readJWT(jwt, encryption, 'ICSlibrary');
-      const userInfo = decrypted.data;
-      setUser(userInfo);
-    } catch (err) {}
-  };
+    // see if there's current user logged in the browser
+    const getCurrentToken = () => {
+        try {
+            const jwt = localStorage.getItem(jwtPrivateKey);
+            const encryption = {
+                key: jwtEncryptionKey,
+                algorithm: "aes-256-cbc",
+            };
+            const decrypted = jwtEncrypt.readJWT(jwt, encryption, "ICSlibrary");
+            const userInfo = decrypted.data;
+            setUser(userInfo);
+        } catch (err) {}
+    };
 
-  // login/register a user
-  const loginRegisterUser = async (userInfo) => {
-    try {
-      const {data} = await personService.loginRegisterUser(userInfo);
-      localStorage.setItem(jwtPrivateKey, data); //set token
-      window.location = "/home";
-    } catch (err) {}
-  };
+    // login/register a user
+    const loginRegisterUser = async (userInfo) => {
+        try {
+            const { data } = await personService.loginRegisterUser(userInfo);
+            localStorage.setItem(jwtPrivateKey, data); //set token
+            window.location = "/home";
+        } catch (err) {}
+    };
 
-  const ParamUrl=()=> {
-    // for dynamic url
-    let { id } = useParams();
+    const ParamUrl = () => {
+        // for dynamic url
+        let { id } = useParams();
+        return null;
+    };
+
     return (
-      null
-    );
-  }
-  
-  return (
-    <div className="App" ref={appRef}>
-      <NavigationBar
-        loginRegisterUser={loginRegisterUser}
-        browseRef={browseRef}
-        user={user}
-      />
-
-        <Switch>
-          <Route
-            path="/home"
-            render={() => (
-              <Homepage
+        <div className="App" ref={appRef}>
+            <NavigationBar
+                loginRegisterUser={loginRegisterUser}
                 browseRef={browseRef}
-                appRef={appRef}
-                latestAcqRef={latestAcqRef}
-                newsRef={newsRef}
-              />
-            )}
-          />
-            {/* this route returns component depending on the route */}
-            {/* add your new route/path here */}
+                user={user}
+            />
 
-            {/* <Route path="/view-user/:googleId" component={ViewUser}></Route> */}
-            {/* <Route path="/account-setting/" component={ViewUser}></Route> */}
-            <Route
+            <Switch>
+                <Route
+                    path="/home"
+                    render={() => (
+                        <Homepage
+                            browseRef={browseRef}
+                            appRef={appRef}
+                            latestAcqRef={latestAcqRef}
+                            newsRef={newsRef}
+                        />
+                    )}
+                />
+                {/* this route returns component depending on the route */}
+                {/* add your new route/path here */}
+
+                {/* <Route path="/view-user/:googleId" component={ViewUser}></Route> */}
+                <Route
+                    path="/account-setting/"
+                    component={ViewUserPage}></Route>
+                {/* <Route
               path="/account-setting/"
-              render={() => <ViewUserPage user={user} />}></Route>
-          <Route path="/home" render={()=><Homepage browseRef={browseRef} appRef={appRef} latestAcqRef={latestAcqRef} newsRef={newsRef}/>}/>
-          <Route exact path="/not-found" component={Notfound}></Route>
-          
-           {/* add your new route/path here */}
-          <Route path="/view-sp-thesis" component={ViewResource}></Route>
-          <Route path="/update-sp-thesis" component={updateResourceData}></Route>
-          <Route path="/manage-resources" component={ManageResPage}></Route>
-          <Route path ="/add-new-book" component={AddBookPage}></Route>
-          <Route path ="/add-new-spt" component={AddSPThesisPage}></Route>
-          <Route path="/delete/:id" children={<ParamUrl />}  component={ViewResource}></Route>
-          <Route path="/manage-users" component={ManageUser}></Route>
-          <Route path="/about" render={()=><About appRef={appRef}/>}/>
-          <Route exact path="/not-found" component={Notfound}></Route> 
-          <Redirect exact from="/" to="/home"/>
-          <Redirect to="/not-found"/>
-        </Switch>
+              render={() => <ViewUserPage user={user} />}></Route> */}
+                <Route
+                    path="/home"
+                    render={() => (
+                        <Homepage
+                            browseRef={browseRef}
+                            appRef={appRef}
+                            latestAcqRef={latestAcqRef}
+                            newsRef={newsRef}
+                        />
+                    )}
+                />
+                <Route exact path="/not-found" component={Notfound}></Route>
 
-      <Footer />
-    </div>
-  );
+                {/* add your new route/path here */}
+                <Route path="/view-sp-thesis" component={ViewResource}></Route>
+                <Route
+                    path="/update-sp-thesis"
+                    component={updateResourceData}></Route>
+                <Route
+                    path="/manage-resources"
+                    component={ManageResPage}></Route>
+                <Route path="/add-new-book" component={AddBookPage}></Route>
+                <Route path="/add-new-spt" component={AddSPThesisPage}></Route>
+                <Route
+                    path="/delete/:id"
+                    children={<ParamUrl />}
+                    component={ViewResource}></Route>
+                <Route path="/manage-users" component={ManageUser}></Route>
+                <Route path="/about" render={() => <About appRef={appRef} />} />
+                <Route exact path="/not-found" component={Notfound}></Route>
+                <Redirect exact from="/" to="/home" />
+                <Redirect to="/not-found" />
+            </Switch>
+
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
--- src/components/viewuserpage/profileContainer.jsx
+++ src/components/viewuserpage/profileContainer.jsx
@@ -1,136 +1,237 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap/";
 import { Button } from "@material-ui/core/";
 import DeleteIcon from "@material-ui/icons/Delete";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck, faPencilAlt } from "@fortawesome/free-solid-svg-icons";
-import jwtDecode from "jwt-decode";
 import { jwtPrivateKey } from "../../config.json";
+import { jwtEncryptionKey } from "../../config.json";
+import * as jwtEncrypt from "jwt-token-encrypt";
 import PersonService from "../../services/personService";
-
-// import { GlobalContext } from "../manageuserpage/userTable";
-import { useHistory } from "react-router-dom";
-import LabelContainer from "./labelContainer";
 
 import "../../styles/userPageStyle.css";
 
-export default function ProfileContainer({ user }) {
-  useEffect(() => {
-    console.log(user);
-  }, []);
-  // removes JWT token from the browser
-  const logout = async () => {
-    try {
-      localStorage.removeItem(jwtPrivateKey);
-      await PersonService.logoutUser(user);
-      window.location = "/";
-    } catch (err) {}
-  };
+export default function ProfileContainer() {
+    const [user, setUser] = useState(null);
 
-  return (
-    // column for the title bar "Profile Display"
-    <Container fixed className="profile-container">
-      <Row className="title-bar">
-        <Col xs={2}></Col>
-        <Col xs={8}>
-          <div className="headerText" style={headerText}>
-            Profile Display
-          </div>
-        </Col>
-        <Col xs={1} className="columns-temp"></Col>
-      </Row>
-      <Row>
-        <Col xs={2}></Col>
-        <Col xs={8} className="columns-temp">
-          <LabelContainer
-            msg={"Nickname: "}
-            userinfos={user.nickname}
-            iseditable={true}
-          />
-        </Col>
-        <Col xs={1} className="columns-temp"></Col>
-      </Row>
-      <Row>
-        <Col xs={2}></Col>
-        <Col xs={8} className="columns-temp">
-          <LabelContainer msg={"Name: "} userinfos={user.fullName} />
-        </Col>
-        <Col xs={1} className="columns-temp"></Col>
-      </Row>
-      <Row>
-        <Col xs={2}></Col>
-        <Col xs={8} className="columns-temp">
-          <LabelContainer
-            msg={"Classification: "}
-            userinfos={user.userType}
-            isType={true}
-          />
-        </Col>
-        <Col xs={1} className="columns-temp"></Col>
-      </Row>
-      <Row>
-        <Col xs={2}></Col>
-        <Col xs={8} className="columns-temp">
-          <LabelContainer msg={"Email: "} userinfos={user.email} />
-        </Col>
-        <Col xs={1} className="columns-temp"></Col>
-      </Row>
-      {/* part for account removal */}
-      <Row className="removal-bar">
-        <Col xs={2}></Col>
-        <Col xs={8}>
-          <div
-            className="header-text"
-            style={{ fontWeight: "900", fontSize: "30px" }}>
-            Account Removal
-          </div>
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={2}></Col>
-        <Col xs={8} className="columns-temp">
-          <div style={{ padding: "12px", fontSize: "15px" }}>
-            Removing your accounts means dissociating your account from the app.
-          </div>
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={2}></Col>
-        <Col xs={8}>
-          {/* BACKLOG: 
-        - MODAL OR POP-UP TO WARN USER ABOUT DISSOCIATION
-        - ACCOUNT DISSOCIATION (FROM BACKEND(?)) */}
-          <Button
-            onClick={logout}
-            variant="contained"
-            color="secondary"
-            className="delete-button"
-            style={{ paddingLeft: "10px" }}
-            startIcon={<DeleteIcon />}
-            fontWeight="900">
-            Remove Account
-          </Button>
-        </Col>
-      </Row>
-    </Container>
-  );
+    const [type, setType] = useState(null);
+    const [nick, setNick] = useState(null);
+
+    const [click, setClick] = useState(false);
+    const [buttonStyle, setButtonStyle] = useState(faPencilAlt);
+    const [style, setStyle] = useState(editButtonDefault);
+    const [disable, setDisable] = useState(true);
+
+    useEffect(() => {
+        getCurrentToken();
+    }, []);
+
+    // removes JWT token from the browser
+    const logout = async () => {
+        try {
+            localStorage.removeItem(jwtPrivateKey);
+            await PersonService.logoutUser(user);
+            window.location = "/";
+        } catch (err) {}
+    };
+
+    const getCurrentToken = () => {
+        try {
+            const jwt = localStorage.getItem(jwtPrivateKey);
+            const encryption = {
+                key: jwtEncryptionKey,
+                algorithm: "aes-256-cbc",
+            };
+            const decrypted = jwtEncrypt.readJWT(jwt, encryption, "ICSlibrary");
+            const userInfo = decrypted.data;
+            setUser(userInfo);
+            setNick(userInfo && userInfo.nickname);
+            setType(userInfo && userInfo.userType);
+        } catch (err) {}
+    };
+
+    const setIcon = (click, buttonStyle, style) => {
+        setClick(click);
+        setButtonStyle(buttonStyle);
+        setStyle(style);
+        setDisable(!disable);
+    };
+
+    const editNickname = () => {
+        !click
+            ? setIcon(true, faCheck, editButtonConfirm)
+            : setIcon(false, faPencilAlt, editButtonDefault);
+    };
+
+    // convert userType to its corresponding string representation
+    useEffect(() => {
+        // console.log(userinfos);
+        if (user) {
+            if (user.userType === 1) setType("Admin");
+            else if (user.userType === 2) setType("Faculty");
+            else if (user.userType === 3) setType("Staff");
+            else setType("Student");
+        }
+    }, [user]);
+
+    return (
+        // column for the title bar "Profile Display"
+        <Container fixed className="profile-container">
+            <Row className="title-bar">
+                <Col xs={2} className="columns-temp"></Col>
+                <Col xs={8}>
+                    <div className="headerText" style={headerText}>
+                        Profile Display
+                    </div>
+                </Col>
+                <Col xs={1} className="columns-temp"></Col>
+            </Row>
+
+            <Row>
+                <Col xs={2} className="columns-temp"></Col>
+                <Col xs={4} className="columns-temp">
+                    <div class="col-4" className="label-text" style={labelText}>
+                        Nickname:
+                    </div>
+                </Col>
+                <Col xs={4} className="columns-temp">
+                    <input
+                        onChange={(e) => setNick(e.target.value)}
+                        disabled={disable}
+                        type="text"
+                        className="text-field"
+                        value={nick}
+                    />
+                </Col>
+                <Col xs={1} className="edit-column">
+                    <div class="button">
+                        <FontAwesomeIcon
+                            onClick={editNickname}
+                            state={click}
+                            aria-label="edit"
+                            style={style}
+                            icon={buttonStyle}
+                        />
+                    </div>
+                </Col>
+            </Row>
+
+            <Row>
+                <Col xs={2} className="columns-temp"></Col>
+                <Col xs={4} className="columns-temp">
+                    <div class="col-4" className="label-text" style={labelText}>
+                        Name:
+                    </div>
+                </Col>
+                <Col xs={4} className="columns-temp">
+                    <input
+                        disabled={true}
+                        type="text"
+                        className="text-field"
+                        value={user && user.fullName}
+                    />
+                </Col>
+                <Col xs={1} className="columns-temp"></Col>
+            </Row>
+            <Row>
+                <Col xs={2} className="columns-temp"></Col>
+                <Col xs={4} className="columns-temp">
+                    <div class="col-4" className="label-text" style={labelText}>
+                        Classification:
+                    </div>
+                </Col>
+                <Col xs={4} className="columns-temp">
+                    <input
+                        type="text"
+                        disabled={true}
+                        className="text-field"
+                        value={type}
+                        // value={user && user.userType}
+                    />
+                </Col>
+                <Col xs={1} className="columns-temp"></Col>
+            </Row>
+            <Row>
+                <Col xs={2} className="columns-temp"></Col>
+                <Col xs={4} className="columns-temp">
+                    <div class="col-4" className="label-text" style={labelText}>
+                        Email:
+                    </div>
+                </Col>
+                <Col xs={4} className="columns-temp">
+                    <input
+                        disabled={true}
+                        type="text"
+                        className="text-field"
+                        value={user && user.email}
+                    />
+                </Col>
+                <Col xs={1} className="columns-temp"></Col>
+            </Row>
+            {/* part for account removal */}
+            <Row className="removal-bar">
+                <Col xs={2} className="columns-temp"></Col>
+                <Col xs={8}>
+                    <div
+                        className="header-text"
+                        style={{ fontWeight: "900", fontSize: "30px" }}>
+                        Account Removal
+                    </div>
+                </Col>
+            </Row>
+            <Row>
+                <Col xs={2} className="columns-temp"></Col>
+                <Col xs={8} className="columns-temp">
+                    <div style={{ padding: "12px", fontSize: "15px" }}>
+                        Removing your accounts means dissociating your account
+                        from the app.
+                    </div>
+                </Col>
+            </Row>
+            <Row>
+                <Col xs={2} className="columns-temp"></Col>
+                <Col xs={8}>
+                    {/* BACKLOG:
+            - MODAL OR POP-UP TO WARN USER ABOUT DISSOCIATION
+            - ACCOUNT DISSOCIATION (FROM BACKEND(?)) */}
+                    <Button
+                        onClick={logout}
+                        variant="contained"
+                        color="secondary"
+                        className="delete-button"
+                        style={{ paddingLeft: "10px" }}
+                        startIcon={<DeleteIcon />}
+                        fontWeight="900">
+                        Remove Account
+                    </Button>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
 const headerText = {
-  fontWeight: "900",
-  fontSize: "35px",
+    fontWeight: "900",
+    fontSize: "35px",
+};
+
+const labelText = {
+    verticalAlign: "center",
+    paddingTop: "12px",
+    fontWeight: "900",
+    fontSize: "25px",
 };
 
 const editButtonDefault = {
-  color: "gray",
-  margin: "24px 0 0px -30px",
-  width: "20px",
-  height: "20px",
+    color: "gray",
+    // margin: "5px 0 0px 0px",
+    width: "20px",
+    height: "20px",
 };
 
 const editButtonConfirm = {
-  color: "#90ee90",
-  margin: "24px 0 0px -30px",
-  width: "20px",
-  height: "20px",
+    color: "#90ee90",
+    // margin: "5px 0 0px 0px",
+    width: "20px",
+    height: "20px",
 };
