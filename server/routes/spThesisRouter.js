f58b5e470fee00733b0a6383deeed29e5d4e1c47
ryanresoles <rbresoles@up.edu.ph>
2021-05-23 20:18:24 +0800

    Initial commit

--- server/routes/spThesisRouter.js
+++ server/routes/spThesisRouter.js
@@ -433,15 +433,24 @@ router.get("/search", async (req, res)=> {
 router.put("/update-sp-thesis", authAdmin, async (req, res) => {
     const {old_sp_thesis_id, sp_thesis_id, type, title, abstract, year, source_code, manuscript, journal, poster, authors, advisers, keywords} = req.body; 
     
-    
+    if(!sp_thesis_id || !type || !title || !abstract || !year || !source_code || !manuscript ||  !journal || !poster || !advisers || !authors || !keywords){
+        return res.status(400).json({errorMessage: "Please enter all required fields."});
+    }
+
+    // checks if sp_thesis_id inputted already exists
+    await thesisModel.findOne({sp_thesis_id}, (err, checker) => {
+        if(checker) {
+            return res.status(400).json({ errorMessage: "New ID inputted already exists."});
+        }
+    })
 
     try{
         // looks for the sp/thesis based on the json object passed, then updates it
         await thesisModel.findOne({"sp_thesis_id": old_sp_thesis_id}, (err, updatedThesisSp) => {
-            if(!sp_thesis_id || !type || !title || !abstract || !year || !source_code || !manuscript ||  !journal || !poster || !advisers || !authors || !keywords){
-                return res.status(400).json({errorMessage: "Please enter all required fields."});
+            
+            if(!updatedThesisSp){
+                return res.status(400).json({ errorMessage: "Entry not found."});
             }
-
             // changing values
             updatedThesisSp.sp_thesis_id = sp_thesis_id;
             updatedThesisSp.type = type;
