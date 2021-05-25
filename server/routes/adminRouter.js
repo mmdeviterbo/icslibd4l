7770c1ba3de56709bed893478dfb4ced8f31ec48
Elcid Cruzado <escruzado@up.edu.ph>
2021-05-26 05:58:20 +0800

    Code refactor

--- server/routes/adminRouter.js
+++ server/routes/adminRouter.js
@@ -7,154 +7,154 @@ const jwtPrivateKey = config.get("jwtPrivateKey");
 
 //read all admin entries
 router.get("/readAdmins", authAdmin, async (req, res) => {
-  UserModel.find({ userType: 1 }, (err, result) => {
-    //reads all the documents and sends as response
-    if (err) {
-      res.send(err);
-    } else {
-      res.send(result);
-    }
-  });
+    UserModel.find({ userType: 1 }, (err, result) => {
+        //reads all the documents and sends as response
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(result);
+        }
+    });
 });
 
 //read all users
 router.get("/readAllUsers", authAdmin, async (req, res) => {
-  UserModel.find({}, (err, result) => {
-    //reads all the documents and sends as response
-    if (err) {
-      res.send(err);
-    } else {
-      res.send(result);
-    }
-  });
+    UserModel.find({}, (err, result) => {
+        //reads all the documents and sends as response
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(result);
+        }
+    });
 });
 
 //read all users
 router.get("/readUserLogs", authAdmin, async (req, res) => {
-  UserLogModel.find({}, (err, result) => {
-    //reads all the documents and sends as response
-    if (err) {
-      res.send(err);
-    } else {
-      res.send(result);
-    }
-  });
+    UserLogModel.find({}, (err, result) => {
+        //reads all the documents and sends as response
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(result);
+        }
+    });
 });
 
 //update
 router.put("/updateOtherUser", authAdmin, async (req, res) => {
-  try {
-    const { googleId, userType } = req.body; //get googleId and newNickname from body
-    let updatedUser;
+    try {
+        const { googleId, userType } = req.body; //get googleId and newNickname from body
+        let updatedUser;
 
-    if (userType) {
-      //update nickname field
-      await UserModel.updateOne(
-        { googleId: googleId },
-        { userType: userType },
-        {
-          new: true,
+        if (userType) {
+            //update nickname field
+            await UserModel.updateOne(
+                { googleId: googleId },
+                { userType: userType },
+                {
+                    new: true,
+                }
+            );
         }
-      );
-    }
 
-    UserModel.find({ googleId: googleId }, (err, result) => {
-      //send the edited user as response
-      if (err) {
-        res.send(err);
-      } else {
-        res.send(result);
-      }
-    });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send();
-  }
+        UserModel.find({ googleId: googleId }, (err, result) => {
+            //send the edited user as response
+            if (err) {
+                res.send(err);
+            } else {
+                res.send(result);
+            }
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send();
+    }
 });
 
 //search function
-router.get("/search", authAdmin, async (req, res) => {
-  let idList = [];
-  let init_output;
-  let final_output;
+router.get("/search", async (req, res) => {
+    let idList = [];
+    let init_output;
+    let final_output;
 
-  function saveId(item, index) {
-    if (!idList) {
-      idList = item._id;
-    } else {
-      idList = [].concat(idList, item._id);
+    function saveId(item, index) {
+        if (!idList) {
+            idList = item._id;
+        } else {
+            idList = [].concat(idList, item._id);
+        }
     }
-  }
 
-  if (req.query.search) {
-    //seach queries for email, name, and nickname attributes\
-    //googleId
-    init_output = await UserModel.find({
-      googleId: {
-        $regex: req.query.search,
-        $options: "i",
-      },
-    });
-    //add to final list
-    final_output = init_output;
-    console.log(final_output);
-    //add _id to idList
-    init_output.forEach(saveId);
-    //email
-    init_output = await UserModel.find({
-      email: {
-        $regex: req.query.search,
-        $options: "i",
-      },
-    });
-    //add to final list
-    final_output = [].concat(final_output, init_output);
-    console.log(final_output);
-    //add _id to idList
-    init_output.forEach(saveId);
+    if (req.query.search) {
+        //seach queries for email, name, and nickname attributes\
+        //googleId
+        init_output = await UserModel.find({
+            googleId: {
+                $regex: req.query.search,
+                $options: "i",
+            },
+        });
+        //add to final list
+        final_output = init_output;
+        console.log(final_output);
+        //add _id to idList
+        init_output.forEach(saveId);
+        //email
+        init_output = await UserModel.find({
+            email: {
+                $regex: req.query.search,
+                $options: "i",
+            },
+        });
+        //add to final list
+        final_output = [].concat(final_output, init_output);
+        console.log(final_output);
+        //add _id to idList
+        init_output.forEach(saveId);
 
-    //fullName
-    init_output = await UserModel.find({
-      fullName: {
-        $regex: req.query.search,
-        $options: "i",
-      },
+        //fullName
+        init_output = await UserModel.find({
+            fullName: {
+                $regex: req.query.search,
+                $options: "i",
+            },
 
-      _id: {
-        $nin: idList,
-      },
-    });
-    //add to final list
-    final_output = [].concat(final_output, init_output);
-    console.log(final_output);
-    //add _id to idList
-    init_output.forEach(saveId);
-    console.log(idList);
+            _id: {
+                $nin: idList,
+            },
+        });
+        //add to final list
+        final_output = [].concat(final_output, init_output);
+        console.log(final_output);
+        //add _id to idList
+        init_output.forEach(saveId);
+        console.log(idList);
 
-    //nickName
-    init_output = await UserModel.find({
-      nickName: {
-        $regex: req.query.search,
-        $options: "i",
-      },
+        //nickName
+        init_output = await UserModel.find({
+            nickName: {
+                $regex: req.query.search,
+                $options: "i",
+            },
 
-      _id: {
-        $nin: idList,
-      },
-    });
-    //add to final list
-    final_output = [].concat(final_output, init_output);
-    //add _id to idList
-    init_output.forEach(saveId);
-  } else {
-    final_output = await UserModel.find();
-  }
-  try {
-    res.send(final_output);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send("Error Getting query");
-  }
+            _id: {
+                $nin: idList,
+            },
+        });
+        //add to final list
+        final_output = [].concat(final_output, init_output);
+        //add _id to idList
+        init_output.forEach(saveId);
+    } else {
+        final_output = await UserModel.find();
+    }
+    try {
+        res.send(final_output);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Error Getting query");
+    }
 });
 
 module.exports = router;
--- src/components/viewuserpage/profileContainer.jsx
+++ src/components/viewuserpage/profileContainer.jsx
@@ -11,6 +11,17 @@ import PersonService from "../../services/personService";
 
 import "../../styles/userPageStyle.css";
 
+const getCurrentToken = () => {
+    const jwt = localStorage.getItem("icslib-privateKey");
+    const encryption = {
+        key: jwtEncryptionKey,
+        algorithm: "aes-256-cbc",
+    };
+    const decrypted = jwtEncrypt.readJWT(jwt, encryption, "ICSlibrary");
+    const userInfo = decrypted.data;
+    return userInfo;
+};
+
 //<summary>
 // create a container that contains user information.
 // user information includes their nickname, full name, classification, and email
@@ -18,278 +29,263 @@ import "../../styles/userPageStyle.css";
 //<output>
 // A component that contains the information regarding the current user
 //</output>
-
-const getCurrentToken = () => {
-  const jwt = localStorage.getItem("icslib-privateKey");
-  const encryption = {
-    key: jwtEncryptionKey,
-    algorithm: "aes-256-cbc",
-  };
-  const decrypted = jwtEncrypt.readJWT(jwt, encryption, "ICSlibrary");
-  const userInfo = decrypted.data;
-  return userInfo;
-};
-
 export default function ProfileContainer() {
-  const [user, setUser] = useState(getCurrentToken());
-  const [user2, setUser2] = useState(null);
+    const [user, setUser] = useState(getCurrentToken());
+    const [user2, setUser2] = useState(null);
 
-  const [type, setType] = useState(null);
-  const [nick, setNick] = useState(null);
+    const [type, setType] = useState(null);
+    const [nick, setNick] = useState(null);
 
-  const [click, setClick] = useState(false);
-  const [buttonStyle, setButtonStyle] = useState(faPencilAlt);
-  const [btnStyle, setBtnStyle] = useState(editButtonDefault);
-  const [disable, setDisable] = useState(true);
+    const [click, setClick] = useState(false);
+    const [buttonStyle, setButtonStyle] = useState(faPencilAlt);
+    const [btnStyle, setBtnStyle] = useState(editButtonDefault);
+    const [disable, setDisable] = useState(true);
 
-  useEffect(() => {
-    // getCurrentToken();
-    console.log(user);
-    setNick(user && user.nickname);
-  }, []);
-  // console.log(user2);
+    useEffect(() => {
+        // getCurrentToken();
+        console.log(user);
+        setNick(user && user.nickname);
+    }, []);
 
-  // removes JWT token from the browser
-  const logout = async () => {
-    try {
-      localStorage.removeItem(jwtPrivateKey);
-      await PersonService.logoutUser(user);
-      window.location = "/";
-    } catch (err) {}
-  };
+    // removes JWT token from the browser
+    const logout = async () => {
+        try {
+            localStorage.removeItem(jwtPrivateKey);
+            await PersonService.logoutUser(user);
+            window.location = "/";
+        } catch (err) {}
+    };
 
-  // const getCurrentToken = () => {
-  //   try {
-  //     const jwt = localStorage.getItem("icslib-privateKey");
-  //     const encryption = {
-  //       key: jwtEncryptionKey,
-  //       algorithm: "aes-256-cbc",
-  //     };
-  //     const decrypted = jwtEncrypt.readJWT(jwt, encryption, "ICSlibrary");
-  //     const userInfo = decrypted.data;
-  //     setUser(userInfo);
+    // const getCurrentToken = () => {
+    //   try {
+    //     const jwt = localStorage.getItem("icslib-privateKey");
+    //     const encryption = {
+    //       key: jwtEncryptionKey,
+    //       algorithm: "aes-256-cbc",
+    //     };
+    //     const decrypted = jwtEncrypt.readJWT(jwt, encryption, "ICSlibrary");
+    //     const userInfo = decrypted.data;
+    //     setUser(userInfo);
 
-  //   } catch (err) {}
-  // };
+    //   } catch (err) {}
+    // };
 
-  const setIcon = (click, buttonStyle, style) => {
-    setClick(click);
-    setButtonStyle(buttonStyle);
-    setBtnStyle(style);
-    setDisable(!disable);
-  };
+    const setIcon = (click, buttonStyle, style) => {
+        setClick(click);
+        setButtonStyle(buttonStyle);
+        setBtnStyle(style);
+        setDisable(!disable);
+    };
 
-  const updateNick = async (userInfo) => {
-    console.log("i am updating");
-    try {
-      const { data } = await PersonService.updateNickname(userInfo);
-    } catch (err) {}
-  };
+    const updateNick = async (userInfo) => {
+        console.log("i am updating");
+        try {
+            const { data } = await PersonService.updateNickname(userInfo);
+        } catch (err) {}
+    };
 
-  const editNickname = (userInfo) => {
-    if (click === false) {
-      setIcon(true, faCheck, editButtonConfirm);
-    } else if (click === true) {
-      setIcon(false, faPencilAlt, editButtonDefault);
-      updateNick(userInfo);
-    }
-  };
+    const editNickname = (userInfo) => {
+        if (click === false) {
+            setIcon(true, faCheck, editButtonConfirm);
+        } else if (click === true) {
+            setIcon(false, faPencilAlt, editButtonDefault);
+            updateNick(userInfo);
+        }
+    };
 
-  // convert userType to its corresponding string representation
-  useEffect(() => {
-    if (user) {
-      if (user.userType === 1) setType("Admin");
-      else if (user.userType === 2) setType("Faculty");
-      else if (user.userType === 3) setType("Staff");
-      else setType("Student");
-    }
-  }, [user]);
+    // convert userType to its corresponding string representation
+    useEffect(() => {
+        if (user) {
+            if (user.userType === 1) setType("Admin");
+            else if (user.userType === 2) setType("Faculty");
+            else if (user.userType === 3) setType("Staff");
+            else setType("Student");
+        }
+    }, [user]);
 
-  useEffect(() => {
-    setUser({
-      ...user,
-      nickname: nick,
-    });
-    console.log(user.nickname);
-  }, [nick]);
+    useEffect(() => {
+        setUser({
+            ...user,
+            nickname: nick,
+        });
+        console.log(user.nickname);
+    }, [nick]);
 
-  useEffect(async () => {
-    console.log("do u exist", user);
-    console.log(user.googleId);
-    try {
-      const { data } = await PersonService.readUser(user.googleId);
-      setUser2(data);
-    } catch (err) {
-      console.log(err);
-    }
+    useEffect(async () => {
+        console.log("do u exist", user);
+        console.log(user.googleId);
+        try {
+            const { data } = await PersonService.readUser(user.googleId);
+            setUser2(data);
+        } catch (err) {
+            console.log(err);
+        }
+        console.log("user2: ", user2);
+    }, []);
     console.log("user2: ", user2);
-  }, []);
-  console.log("user2: ", user2);
 
-  return (
-    // column for the title bar "Profile Display"
-    <Container fixed className="profile-container">
-      <Row className="title-bar">
-        <Col xs={2} className="columns-temp"></Col>
-        <Col xs={8}>
-          <div className="headerText" style={headerText}>
-            Profile Display
-          </div>
-        </Col>
-        <Col xs={1} className="columns-temp"></Col>
-      </Row>
+    return (
+        // column for the title bar "Profile Display"
+        <Container fixed className="profile-container">
+            <Row className="title-bar">
+                <Col xs={2} className="columns-temp"></Col>
+                <Col xs={8}>
+                    <div className="headerText" style={headerText}>
+                        Profile Display
+                    </div>
+                </Col>
+                <Col xs={1} className="columns-temp"></Col>
+            </Row>
 
-      <Row>
-        <Col xs={2} className="columns-temp"></Col>
-        <Col xs={4} className="columns-temp">
-          <div class="col-4" className="label-text" style={labelText}>
-            Nickname:
-          </div>
-        </Col>
-        <Col xs={4} className="columns-temp">
-          <input
-            onChange={(e) => {
-              setNick(e.target.value);
-            }}
-            disabled={disable}
-            type="text"
-            className="text-field"
-            value={nick}
-          />
-        </Col>
-        <Col xs={1} className="edit-column">
-          <div class="button">
-            <FontAwesomeIcon
-              onClick={() => {
-                editNickname({
-                  googleId: user.googleId,
-                  newNickname: user.nickname,
-                });
-              }}
-              state={click}
-              aria-label="edit"
-              style={btnStyle}
-              icon={buttonStyle}
-            />
-          </div>
-        </Col>
-      </Row>
+            <Row>
+                <Col xs={2} className="columns-temp"></Col>
+                <Col xs={4} className="columns-temp">
+                    <div class="col-4" className="label-text" style={labelText}>
+                        Nickname:
+                    </div>
+                </Col>
+                <Col xs={4} className="columns-temp">
+                    <input
+                        onChange={(e) => {
+                            setNick(e.target.value);
+                        }}
+                        disabled={disable}
+                        type="text"
+                        className="text-field"
+                        value={nick}
+                    />
+                </Col>
+                <Col xs={1} className="edit-column">
+                    <div class="button">
+                        <FontAwesomeIcon
+                            onClick={() => {
+                                editNickname({
+                                    googleId: user.googleId,
+                                    newNickname: user.nickname,
+                                });
+                            }}
+                            state={click}
+                            aria-label="edit"
+                            style={btnStyle}
+                            icon={buttonStyle}
+                        />
+                    </div>
+                </Col>
+            </Row>
 
-      <Row>
-        <Col xs={2} className="columns-temp"></Col>
-        <Col xs={4} className="columns-temp">
-          <div class="col-4" className="label-text" style={labelText}>
-            Name:
-          </div>
-        </Col>
-        <Col xs={4} className="columns-temp">
-          <input
-            disabled={true}
-            type="text"
-            className="text-field"
-            value={user && user.fullName}
-          />
-        </Col>
-        <Col xs={1} className="columns-temp"></Col>
-      </Row>
-      <Row>
-        <Col xs={2} className="columns-temp"></Col>
-        <Col xs={4} className="columns-temp">
-          <div class="col-4" className="label-text" style={labelText}>
-            Classification:
-          </div>
-        </Col>
-        <Col xs={4} className="columns-temp">
-          <input
-            type="text"
-            disabled={true}
-            className="text-field"
-            value={type}
-            // value={user && user.userType}
-          />
-        </Col>
-        <Col xs={1} className="columns-temp"></Col>
-      </Row>
-      <Row>
-        <Col xs={2} className="columns-temp"></Col>
-        <Col xs={4} className="columns-temp">
-          <div class="col-4" className="label-text" style={labelText}>
-            Email:
-          </div>
-        </Col>
-        <Col xs={4} className="columns-temp">
-          <input
-            disabled={true}
-            type="text"
-            className="text-field"
-            value={user && user.email}
-          />
-        </Col>
-        <Col xs={1} className="columns-temp"></Col>
-      </Row>
-      {/* part for account removal */}
-      <Row className="removal-bar">
-        <Col xs={2} className="columns-temp"></Col>
-        <Col xs={8}>
-          <div
-            className="header-text"
-            style={{ fontWeight: "900", fontSize: "30px" }}>
-            Account Removal
-          </div>
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={2} className="columns-temp"></Col>
-        <Col xs={8} className="columns-temp">
-          <div style={{ padding: "12px", fontSize: "15px" }}>
-            Removing your accounts means dissociating your account from the app.
-          </div>
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={2} className="columns-temp"></Col>
-        <Col xs={8}>
-          {/* BACKLOG:
-            - MODAL OR POP-UP TO WARN USER ABOUT DISSOCIATION
-            - ACCOUNT DISSOCIATION (FROM BACKEND(?)) */}
-          <Button
-            onClick={logout}
-            variant="contained"
-            color="secondary"
-            className="delete-button"
-            style={{ paddingLeft: "10px" }}
-            startIcon={<DeleteIcon />}
-            fontWeight="900">
-            Remove Account
-          </Button>
-        </Col>
-      </Row>
-    </Container>
-  );
+            <Row>
+                <Col xs={2} className="columns-temp"></Col>
+                <Col xs={4} className="columns-temp">
+                    <div class="col-4" className="label-text" style={labelText}>
+                        Name:
+                    </div>
+                </Col>
+                <Col xs={4} className="columns-temp">
+                    <input
+                        disabled={true}
+                        type="text"
+                        className="text-field"
+                        value={user && user.fullName}
+                    />
+                </Col>
+                <Col xs={1} className="columns-temp"></Col>
+            </Row>
+            <Row>
+                <Col xs={2} className="columns-temp"></Col>
+                <Col xs={4} className="columns-temp">
+                    <div class="col-4" className="label-text" style={labelText}>
+                        Classification:
+                    </div>
+                </Col>
+                <Col xs={4} className="columns-temp">
+                    <input
+                        type="text"
+                        disabled={true}
+                        className="text-field"
+                        value={type}
+                        // value={user && user.userType}
+                    />
+                </Col>
+                <Col xs={1} className="columns-temp"></Col>
+            </Row>
+            <Row>
+                <Col xs={2} className="columns-temp"></Col>
+                <Col xs={4} className="columns-temp">
+                    <div class="col-4" className="label-text" style={labelText}>
+                        Email:
+                    </div>
+                </Col>
+                <Col xs={4} className="columns-temp">
+                    <input
+                        disabled={true}
+                        type="text"
+                        className="text-field"
+                        value={user && user.email}
+                    />
+                </Col>
+                <Col xs={1} className="columns-temp"></Col>
+            </Row>
+            {/* part for account removal */}
+            <Row className="removal-bar">
+                <Col xs={2} className="columns-temp"></Col>
+                <Col xs={8}>
+                    <div
+                        className="header-text"
+                        style={{ fontWeight: "900", fontSize: "30px" }}>
+                        Account Removal
+                    </div>
+                </Col>
+            </Row>
+            <Row>
+                <Col xs={2} className="columns-temp"></Col>
+                <Col xs={8} className="columns-temp">
+                    <div style={{ padding: "12px", fontSize: "15px" }}>
+                        Removing your accounts means dissociating your account
+                        from the app.
+                    </div>
+                </Col>
+            </Row>
+            <Row>
+                <Col xs={2} className="columns-temp"></Col>
+                <Col xs={8}>
+                    <Button
+                        onClick={logout}
+                        variant="contained"
+                        color="secondary"
+                        className="delete-button"
+                        style={{ paddingLeft: "10px" }}
+                        startIcon={<DeleteIcon />}
+                        fontWeight="900">
+                        Remove Account
+                    </Button>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
 const headerText = {
-  fontWeight: "900",
-  fontSize: "35px",
+    fontWeight: "900",
+    fontSize: "35px",
 };
 
 const labelText = {
-  verticalAlign: "center",
-  paddingTop: "12px",
-  fontWeight: "900",
-  fontSize: "25px",
+    verticalAlign: "center",
+    paddingTop: "12px",
+    fontWeight: "900",
+    fontSize: "25px",
 };
 
 const editButtonDefault = {
-  color: "gray",
-  // margin: "5px 0 0px 0px",
-  width: "20px",
-  height: "20px",
+    color: "gray",
+    // margin: "5px 0 0px 0px",
+    width: "20px",
+    height: "20px",
 };
 
 const editButtonConfirm = {
-  color: "#90ee90",
-  // margin: "5px 0 0px 0px",
-  width: "20px",
-  height: "20px",
+    color: "#90ee90",
+    // margin: "5px 0 0px 0px",
+    width: "20px",
+    height: "20px",
 };
