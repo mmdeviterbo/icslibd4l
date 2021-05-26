4966a22711ad65ef25e9f0e9a764ac0a13743b89
mewguel <iamalabanan@up.edu.ph>
2021-05-26 17:16:44 +0800

    design updates margin and padding

--- src/components/searchResult/searchBar.jsx
+++ src/components/searchResult/searchBar.jsx
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import React, {useState} from 'react';
-
-// import styled from 'styled-components';
-
-export default function SearchBar(){
-
-    return (
-        <input type='search' 
-        className='form-control removeOutline'
-        placeholder= {"Search"}
-        style={searchInput}
-        />
-    )
-}
-
-const searchInput = {
-    border: "0",
-    borderRadius: "10px",
-    padding: "5px",
-    textIndent: "10px",
-    width: "80%",    
-=======
 import React from 'react';
 
 export default function SearchBar({searchFilter, setSearchFilter}){
@@ -41,5 +18,4 @@ const searchInput = {
     padding: "0.38vw",
     textIndent: "0.76vw",
     width: "95.3%",
->>>>>>> a0b5063ab401ce4ac3fe318e93aee75a547a07bd
 }
