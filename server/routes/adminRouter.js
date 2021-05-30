68864a9d6417d667e2eec3dc3a320ee33781d0bf
johncaleb <jccanlas3@up.edu.ph>
2021-05-30 17:24:16 +0800

    create and download pdf at admin

--- server/routes/adminRouter.js
+++ server/routes/adminRouter.js
@@ -3,6 +3,10 @@ const UserModel = require("../models/userModel");
 const UserLogModel = require("../models/userLogModel");
 const authAdmin = require("../middleware/authAdmin");
 
+//for generating reports
+const puppeteer = require('puppeteer');
+const fs = require('fs-extra');
+
 //read all admin entries
 /**************************************************** 
 Request Object:
@@ -240,5 +244,29 @@ router.get("/search", async (req, res) => {
         res.status(500).send("Error Getting query");
     }
 });
+
+router.get("/report", async (req, res) => {
+    try {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+
+        await page.setContent('<h1>hello</h1>');
+        await page.emulateMediaType('screen');
+        await page.pdf({
+            path: 'sample.pdf',
+            format: 'A4',
+            printBackground: true
+        });
+
+        console.log('done');
+        await browser.close();
+
+        res.send();
+    } catch (err) {
+        console.error(err);
+        res.status(500).send();
+    }
+});
+
 
 module.exports = router;
