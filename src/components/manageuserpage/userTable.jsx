d01770bd295cbc277464a1a2608c0d4c1886f796
johnmelr <jrramos7@up.edu.ph>
2021-05-31 15:11:58 +0800

    Used async function for reading data from database. Organized variable and function declaration.

--- src/components/manageuserpage/userTable.jsx
+++ src/components/manageuserpage/userTable.jsx
@@ -1,4 +1,4 @@
-import React, { useState, createContext, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
@@ -10,6 +10,7 @@ import Paper from "@material-ui/core/Paper";
 import { makeStyles } from "@material-ui/core/styles";
 import { Link, useLocation } from "react-router-dom";
 import PersonService from "../../services/personService";
+import { Select } from "@material-ui/core";
 
 const tableHeader = [
   "User ID",
@@ -22,48 +23,47 @@ const tableHeader = [
 
 let tableEntry = [];
 
-const initialState = {
-  users: [tableEntry],
-};
+// const initialState = {
+//   users: [tableEntry],
+// };
 
-// Data can be used in view user page
-export const GlobalContext = createContext(initialState);
-
-export default function UserTable({user}) {
-
-  const [ userList, setUserList ] = useState([]);
-  const location = useLocation();
-
-
-  useEffect(() => {
-    PersonService.readAllUsers().then((response) => {
-      setUserList(Array.from(response.data));
-    });
-  });
+export default function UserTable({ user }) {
+  // Array for user data retreived from database.
+  const [userList, setUserList] = useState([]);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [isEditing, setIsEditing] = useState(false);
 
   tableEntry = userList;
 
-  // Array for user data retreived from database.
- 
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(10);
   // Computes the number of rows missing in a 10 per item pagination
-  const emptyRows = rowsPerPage - Math.min(rowsPerPage, tableEntry.length - page * rowsPerPage);
+  const emptyRows =
+    rowsPerPage - Math.min(rowsPerPage, tableEntry.length - page * rowsPerPage);
 
+  const classificationString = ["Admin", "Faculty", "Staff", "Student"];
+  const location = useLocation();
+
+  useEffect(() => {
+    readUsers();
+  }, [user]);
+
+  const readUsers = async () => {
+    try {
+      await PersonService.readAllUsers().then((response) => {
+        setUserList(Array.from(response.data));
+      });
+    } catch (err) {}
+  };
+
+  // Handler event for page change in user table
   const handlePageChange = (event, newPage) => {
     setPage(newPage);
   };
 
-  // Handler event for page change in user table
   const handleChangeRowsPerPage = (event) => {
     setRowsPerPage(parseInt(event.target.value, 10));
     setPage(0);
   };
-
-  // Column Header
-  const header = tableHeader.map((header_text, index) => (
-    <TableCell key={index} style={{ fontWeight: "bolder" }}><span>{header_text}</span></TableCell>
-  ));
 
   const useStyles = makeStyles({
     root: {
@@ -73,45 +73,80 @@ export default function UserTable({user}) {
 
   const tableContainer = useStyles();
 
-  const entries = tableEntry.map((entry, index) => (
-    <TableRow hover key={entry.googleId} user={entry}>
-      <TableCell style={{ width: "80x", fontWeight: "bold" }}>
-        <span>{entry.googleId}</span>
+  const editClassification = () => {
+    return (
+      <Select
+        className="classification-chuchu"
+        placeholder={"Classification"}
+        options={classificationString}
+      />
+    );
+  };
+
+  // Column Header
+  const header = tableHeader.map((header_text, index) => {
+    return (
+      <TableCell key={index} style={{ fontWeight: "bolder" }}>
+        <span>{header_text}</span>
       </TableCell>
-      <TableCell style={{ align: "left", fontWeight: "bolder", color: "black"}}>
-        <Link to={`/viewuser/${entry.googleId}`}>{entry.fullName}</Link>
-      </TableCell>
-      <TableCell style={{ align: "left", fontWeight: "bolder", color: "#FFFFFF" }}>
-        <Link to={`/viewuser/${entry.googleId}`}>{entry.nickname}</Link>
-      </TableCell>
-      <TableCell style={{ width: "80px" }}>
-        <span>{entry.email}</span>
-      </TableCell>
-      <TableCell style={{ width: "80px", textAlign: "center"}}>
-        <span>{entry.userType}</span>
-      </TableCell>
-      <TableCell style={{ textAlign: "center", fontSize: "1.5rem" }}>
-        <i className="fa fa-ellipsis-h" style={{ margin: "10px", color: "#CFCFCF",  }}></i>
-        <Link to={{
-                pathname: "/manage-users/delete-user",
-                state: {
-                    background: location,
-                    id: entry.googleId,
-                    item: "account",
-                    user: { 
-                            googleId: entry.googleId,
-                            email: entry.email,
-                            fullName: entry.fullName,
-                            nickName: entry.nickname,
-                            userType: entry.userType,
-                         },
+    );
+  });
+
+  const entries = tableEntry.map((entry, index) => {
+    return (
+      <TableRow hover key={entry.googleId} user={entry}>
+        <TableCell style={{ width: "80x", fontWeight: "bold" }}>
+          <span>{entry.googleId}</span>
+        </TableCell>
+        <TableCell
+          style={{ align: "left", fontWeight: "bolder", color: "black" }}
+        >
+          <Link to={`/viewuser/${entry.googleId}`}>{entry.fullName}</Link>
+        </TableCell>
+        <TableCell
+          style={{ align: "left", fontWeight: "bolder", color: "#FFFFFF" }}
+        >
+          <Link to={`/viewuser/${entry.googleId}`}>{entry.nickname}</Link>
+        </TableCell>
+        <TableCell style={{ width: "80px" }}>
+          <span>{entry.email}</span>
+        </TableCell>
+        <TableCell style={{ width: "80px", textAlign: "center" }}>
+          <span className="user-classification-cell">
+            {classificationString[entry.userType - 1]}
+          </span>
+        </TableCell>
+        <TableCell style={{ textAlign: "center", fontSize: "1.5rem" }}>
+          <i
+            className="fa fa-ellipsis-h"
+            style={{ margin: "10px", color: "#CFCFCF" }}
+          ></i>
+          <Link
+            to={{
+              pathname: "/manage-users/delete-user",
+              state: {
+                background: location,
+                id: entry.googleId,
+                item: "user",
+                user: {
+                  googleId: entry.googleId,
+                  email: entry.email,
+                  fullName: entry.fullName,
+                  nickName: entry.nickname,
+                  userType: entry.userType,
                 },
-            }}>
-          <i className="fa fa-trash-o" style={{ margin: "10px", color: "#CFCFCF" }}></i>
-        </Link>
-      </TableCell>
-    </TableRow>
-  ));
+              },
+            }}
+          >
+            <i
+              className="fa fa-trash-o"
+              style={{ margin: "10px", color: "#CFCFCF" }}
+            ></i>
+          </Link>
+        </TableCell>
+      </TableRow>
+    );
+  });
 
   return (
     <>
