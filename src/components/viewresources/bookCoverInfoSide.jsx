d4936dbddddde8c369b8bd26f9c2b1a1bee43678
Elcid Cruzado <escruzado@up.edu.ph>
2021-06-02 20:45:30 +0800

    Revert "Fix: Merge issues"
    This reverts commit 2ed0dedd475fde310387b47abb14cd0009742d73.

--- src/components/viewresources/bookCoverInfoSide.jsx
+++ src/components/viewresources/bookCoverInfoSide.jsx
@@ -1,50 +1,69 @@
 import React from "react";
+// import resourceServices from "../../services/resourceService";
 
-const BookCoverandInfo = ({
-    isbn,
-    publisher,
-    numOfCopies,
-    subjects,
-    physicalDesc,
-}) => {
-    return (
-        <div className="book-cover-info-side">
-            <table id="bookinfo">
-                <tr className="book-info-tr">
-                    {/* <th className = "book-info-tr"> ISBN </th>
-                    <td className = "book-info-tr"> 123-123456789</td> */}
-                    <th className="book-info-tr"> ISBN: </th>
-                    <th className="book-info-tr"> {isbn} </th>
-                </tr>
-                <tr className="book-info-tr">
-                    {/* <th className = "book-info-tr"> ISBN </th>
-                    <td className = "book-info-tr"> 123-123456789</td> */}
-                    <th className="book-info-tr"> Publisher: </th>
-                    <th className="book-info-tr"> {publisher} </th>
-                </tr>
+const BookCoverandInfo = ({ isbn, publisher, numOfCopies, subjects }) => {
+  console.log(isbn);
 
-                <tr className="book-info-tr">
-                    {/* <th className = "book-info-tr"> ISBN </th>
-                    <td className = "book-info-tr"> 123-123456789</td> */}
-                    <th className="book-info-tr"> Number of copies: </th>
-                    <th className="book-info-tr"> {numOfCopies} </th>
-                </tr>
+  //   useEffect(() => {
+  //     async function getBookCover() {
+  //       try {
+  //         const { data } = await resourceServices.getBookCover(isbn);
+  //         console.log(data);
+  //       } catch (err) {
+  //         if (err.response && err.response.data) {
+  //           alert(err.response.data.errorMessage); // some reason error message
+  //         }
+  //       }
+  //     }
+  //     getBookCover();
+  //   }, []);
 
-                <tr className="book-info-tr">
-                    <th className="book-info-tr"> Subject(s) </th>
-                    {subjects.map((item, key) => (
-                        <th className="book-info-tr" key={key}>
-                            {item.subject}
-                        </th>
-                    ))}
-                    {/* <td className = "book-info-tr"> Agriculture</td> */}
-                </tr>
-            </table>
-            <div className="physical-description">
-                <p className="physical-text">{physicalDesc}</p>
-            </div>
-        </div>
-    );
+  //   const GetBookCover = async () => {
+  //     try {
+  //       const { data } = await resourceServices.getBookCover(isbn);
+  //       console.log(data);
+  //     } catch (err) {
+  //       if (err.response && err.response.data) {
+  //         alert(err.response.data.errorMessage); // some reason error message
+  //       }
+  //     }
+  //   };
+  //   GetBookCover();
+
+  return (
+    <div className="book-cover-info-side">
+      {/* <GetBookCover /> */}
+      <table id="bookinfo">
+        <tr className="bookinfotr">
+          {/* <th className = "bookinfotr"> ISBN </th>
+                    <td className = "bookinfotr"> 123-123456789</td> */}
+          <th className="bookinfotr"> ISBN: </th>
+          <th className="bookinfotr"> {isbn} </th>
+        </tr>
+        <tr className="bookinfotr">
+          {/* <th className = "bookinfotr"> ISBN </th>
+                    <td className = "bookinfotr"> 123-123456789</td> */}
+          <th className="bookinfotr"> Publisher: </th>
+          <th className="bookinfotr"> {publisher} </th>
+        </tr>
+
+        <tr className="bookinfotr">
+          {/* <th className = "bookinfotr"> ISBN </th>
+                    <td className = "bookinfotr"> 123-123456789</td> */}
+          <th className="bookinfotr"> Number of copies: </th>
+          <th className="bookinfotr"> {numOfCopies} </th>
+        </tr>
+
+        <tr className="bookinfotr">
+          <th className="bookinfotr"> Subject(s) </th>
+          {subjects.map((item, key) => (
+            <div key={key}>{item.subject}</div>
+          ))}
+          {/* <td className = "bookinfotr"> Agriculture</td> */}
+        </tr>
+      </table>
+    </div>
+  );
 };
 
 export default BookCoverandInfo;
