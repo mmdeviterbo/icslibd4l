3317ab793e323a8587d9fad98b48e50e1101f202
joayma <jamustapha@up.edu.ph>
2021-06-02 14:38:27 +0800

    Merged from sprint3-ramos

--- src/components/manageresourcespage/resourcesMainTable.jsx
+++ src/components/manageresourcespage/resourcesMainTable.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import PropTypes from "prop-types";
-import clsx from "clsx";
-import { lighten, makeStyles } from "@material-ui/core/styles";
+// import clsx from "clsx";
+import { makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
@@ -11,13 +11,13 @@ import TableHead from "@material-ui/core/TableHead";
 import TablePagination from "@material-ui/core/TablePagination";
 import TableRow from "@material-ui/core/TableRow";
 import TableSortLabel from "@material-ui/core/TableSortLabel";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
+// import Toolbar from "@material-ui/core/Toolbar";
+// import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
-import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
-import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
+// import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+// import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
 import resourceService from "../../services/resourceService";
-import MessagePopUpCont from "../messageModalContainer";
+// import MessagePopUpCont from "../messageModalContainer";
 import dateFormat from "dateformat";
 
 function createResourceData(
