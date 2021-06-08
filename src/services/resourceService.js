da3251bc44d3659817c8030262f052e8c8b03fc3
johnmelr <jrramos7@up.edu.ph>
2021-06-08 14:07:31 +0800

    Added get request for generating summary report.

--- src/services/resourceService.js
+++ src/services/resourceService.js
@@ -5,45 +5,68 @@ import { apiEndpoint } from "../config.json";
 const addSpThesis = (formData) => {
     for (var key of formData.entries()) {
         console.log(key[0] + ", " + key[1]);
-      }
-    return http.post(`${apiEndpoint}/thesis/create`, formData, {withCredentials: true}, {
-        headers: { "Content-Type": "multipart/form-data" }
-    })
-}
+    }
+    return http.post(
+        `${apiEndpoint}/thesis/create`,
+        formData,
+        { withCredentials: true },
+        {
+            headers: { "Content-Type": "multipart/form-data" },
+        }
+    );
+};
 
 const addBook = (formData) => {
     for (var key of formData.entries()) {
         console.log(key[0] + ", " + key[1]);
-      }
-    return http.post(`${apiEndpoint}/books/create`, formData, {withCredentials: true}, {
-        headers: { "Content-Type": "multipart/form-data" }
-    })
-}
+    }
+    return http.post(
+        `${apiEndpoint}/books/create`,
+        formData,
+        { withCredentials: true },
+        {
+            headers: { "Content-Type": "multipart/form-data" },
+        }
+    );
+};
 
 // read data of a resource
 const browseResources = (resourceType) => {
-    console.log(resourceType)
-    return http.post(`${apiEndpoint}/thesis/browse`, resourceType, {withCredentials: true})
-}
+    console.log(resourceType);
+    return http.post(`${apiEndpoint}/thesis/browse`, resourceType, {
+        withCredentials: true,
+    });
+};
 
 const searchSpThesis = (filter, query) => {
-    return http.get(`${apiEndpoint}/thesis${query}`, {params:filter}, {withCredentials: true})
-}
-
+    return http.get(
+        `${apiEndpoint}/thesis${query}`,
+        { params: filter },
+        { withCredentials: true }
+    );
+};
 
 const searchBook = (filter) => {
-    return http.get(`${apiEndpoint}/book/search`, {body:filter}, {withCredentials: true})
-}
-
+    return http.get(
+        `${apiEndpoint}/book/search`,
+        { body: filter },
+        { withCredentials: true }
+    );
+};
 
 const downloadFile = (fileType, query) => {
-    console.log(fileType)
-    console.log(query)
-    return http.get(`${apiEndpoint}/thesis${query}`, {params:fileType}, {
-        withCredentials: true,
-    }, {
-        responseType: 'blob',
-    });
+    console.log(fileType);
+    console.log(query);
+    return http.get(
+        `${apiEndpoint}/thesis${query}`,
+        { params: fileType },
+        {
+            withCredentials: true,
+        },
+        {
+            responseType: "blob",
+        }
+    );
 };
 
 // edit data of a resource
@@ -58,19 +81,21 @@ const editBook = (resourceData) => {
     return http.put(`${apiEndpoint}/book/update-book`, resourceData, {
         withCredentials: true,
     });
-}
+};
 
 // delete book
 // const deleteBook = (deleteId) => {
-//     return http.delete(`${apiEndpoint}/book/delete/$(deleteId)`, 
+//     return http.delete(`${apiEndpoint}/book/delete/$(deleteId)`,
 //     {withCredentials: true})
 
 // }
 
 //delete resource
 const deleteSpThesis = (deleteId) => {
-    return http.delete(`${apiEndpoint}/thesis/remove-sp-thesis/${deleteId}`, {withCredentials: true})
-}
+    return http.delete(`${apiEndpoint}/thesis/remove-sp-thesis/${deleteId}`, {
+        withCredentials: true,
+    });
+};
 
 // get news from uplb news website
 function getNews() {
@@ -78,30 +103,41 @@ function getNews() {
 }
 
 // get all books (object of information only, not images), sorted by date (latest acquisition feature)
-function getBooks(){
+function getBooks() {
     return http.get(`${apiEndpoint}/books/display_infos`);
 }
 
 // get all books (object of images), sorted by date (latest acquisition feature)
-function getBookCovers(){
+function getBookCovers() {
     return http.get(`${apiEndpoint}/books/display_covers`);
 }
-
 
 function getAllResources() {
     return http.get(`${apiEndpoint}/thesis/search`);
 }
 
-function getSPTFiles({ title, fileType }){
-    return http.post(`${apiEndpoint}/thesis/download`, { title, fileType }, {withCredentials: true}, {
-        responseType: 'stream'
+function getSPTFiles({ title, fileType }) {
+    return http.post(
+        `${apiEndpoint}/thesis/download`,
+        { title, fileType },
+        { withCredentials: true },
+        {
+            responseType: "stream",
+        }
+    );
+}
+
+function getBookCover(resourceId) {
+    console.log(resourceId);
+    return http.post(`${apiEndpoint}/books/download1`, resourceId, {
+        withCredentials: true,
     });
 }
 
-function getBookCover(resourceId){
-    console.log(resourceId)
-    return http.post(`${apiEndpoint}/books/download1`, resourceId, {withCredentials: true});
-}
+// read summary report data
+const generateReport = (resourceType) => {
+    return http.get(`${apiEndpoint}/admin/report?type=${resourceType}`);
+};
 
 // put here your newly made functions to export, then "exportFunctions" itself will be the one to be exported
 const exportFunctions = {
@@ -120,7 +156,8 @@ const exportFunctions = {
     getSPTFiles,
     getBookCover,
     downloadFile,
-    getBookCovers
-}
+    getBookCovers,
+    generateReport,
+};
 
 export default exportFunctions;
