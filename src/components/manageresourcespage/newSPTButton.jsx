4be977f00a67b16ec0df6ddf6cb396db072940f9
Elcid Cruzado <escruzado@up.edu.ph>
2021-06-13 06:47:48 +0800

    Chore: made buttons consistent

--- src/components/manageresourcespage/newSPTButton.jsx
+++ src/components/manageresourcespage/newSPTButton.jsx
@@ -5,18 +5,6 @@ const AddNewSPT = () => {
         <div className="new-res-container">
             <a href="/add-new-spt">
                 <button className="new-res-button">
-                    <span className="res-icon">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="23"
-                            height="23"
-                            fill="currentColor"
-                            className="bi bi-plus"
-                            viewBox="0 0 16 16">
-                            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
-                        </svg>
-                    </span>
-
                     <span className="res-btn-txt">Add SP / Thesis</span>
                 </button>
             </a>
