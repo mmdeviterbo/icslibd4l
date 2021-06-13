135e462eb11ea61a68a6fbb545fddb4e89aa7ed8
Marty de Viterbo <mjennety@gmail.com>
2021-06-13 23:38:55 +0800

    REFACTOR: update about component

---
+++ src/components/about/aboutAlternative.jsx
@@ -0,0 +1,40 @@
+import React, {useEffect} from 'react'
+import logo from '../../assets/mainlogo/icslibd4l.png'
+import {gsap} from 'gsap';
+import '../../styles/aboutStyle/aboutStyle.css';
+
+export default function AboutAlternative() {
+
+    useEffect(()=>{
+        gsap.from('.logoAboutClass',{
+            duration:0.8,
+            scale:0.9,
+        })
+    },[])
+
+    return (
+        <div className="aboutMainContainer">
+            <div className="bookContainer">
+                <div className="leftHalfContainer">
+                    <img className="logoAboutClass" src={logo} alt="#"/>
+                    <div className="textHolders">
+                        <h1 style={{letterSpacing:"2px"}}>Analytica</h1>
+                        <p>A Digital Library</p>
+                    </div>
+                </div>
+                <div className="rightHalfContainer">
+                    <img className="logoBgClass" src={logo} alt="#"/>
+                    <div className="aboutTextContainer">
+                        <p className="paragraphTextStyle">
+                            The <strong>Analytica Digital Library</strong> is made for educational purposes only. Unoticed duplication or reproduction of materials is probihited without any permission from the UPLB ICS faculty.   
+                        </p>
+                        <p className="footerTextStyle">
+                            &#169; CMSC 128. Introduction to Software Engineering. <br/>Section D-4L 2021
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
