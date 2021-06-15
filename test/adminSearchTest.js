d77d0e0865114a6cd12b241002505b5c7bd64064
johncaleb <jccanlas3@up.edu.ph>
2021-06-16 02:21:24 +0800

    added unit testing for search user in admin router

---
+++ test/adminSearchTest.js
@@ -0,0 +1,37 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const axios = require("axios");
+const { query } = require("express");
+const expect = chai.expect;
+const userRoute = "http://localhost:3001/users";
+const adminRoute = "http://localhost:3001/admin";
+
+//Assertion style
+chai.use(chaiHttp);
+let token;
+describe("admin router", () => {
+    /**********************
+     Read user route
+     ***********************/
+    // eslint-disable-next-line no-undef
+    before(() => {});
+
+    describe("GET /search ", () => {
+        it("find a user using a keyword", (done) => {
+            axios
+                
+                .get(adminRoute + "/search", {params: {search: "caleb"} }, {
+                    withCredentials: true,
+                })
+                
+                .then((res) => {
+                    expect(res.data).to.be.an("array");
+                    expect(res.status).to.equal(200);
+                    done();
+                })
+                .catch((err) => {
+                    done(err);
+                });
+        });
+    });
+});
