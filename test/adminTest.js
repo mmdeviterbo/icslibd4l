229f95dba14ce2177dea18285ee12233e02a5760
Bien Custodio <bbcustodio2>
2021-06-15 10:54:33 +0800

    ADD: added test files for user log and admin

---
+++ test/adminTest.js
@@ -0,0 +1,53 @@
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+const axios = require("axios");
+const sinon = require("sinon");
+const expect = chai.expect;
+const userLogRoute = "http://localhost:3001/userLogs";
+var app;
+var agent;
+//Assertion style
+chai.use(chaiHttp);
+let token;
+describe("UserLogs Router API", () => {
+    /**********************
+     Read route
+     ***********************/
+    describe("READ /readUserLogs ", () => {
+        it("Delete user information", (done) => {
+            let userInfo = {
+                googleId: "test1234",
+            };
+            axios
+                .get(userLogRoute + "/readUserLogs")
+                .then((res) => {
+                    expect(res.data).to.be.an("array");
+                    expect(res.status).to.equal(200);
+                    done();
+                })
+                .catch((err) => {
+                    done(err);
+                });
+        });
+    });
+    /**********************
+     Delete route
+     ***********************/
+    describe("DELETE /deleteAllUserLogs", () => {
+        it("Delete user information", (done) => {
+            let userInfo = {
+                googleId: "test1234",
+            };
+            axios
+                .delete(userLogRoute + "/deleteAllUserLogs")
+                .then((res) => {
+                    expect(res.status).to.equal(200);
+                    expect(res.data).to.have.string("All Entries Deleted");
+                    done();
+                })
+                .catch((err) => {
+                    done(err);
+                });
+        });
+    });
+});
