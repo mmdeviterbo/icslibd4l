e07cdee158b665d2d17fd692c639e65cb0567f87
rpcolina <rpcolina@up.edu.ph>
2021-06-15 20:50:10 +0800

    book info at the side converted to div instead of table

--- src/components/viewresources/bookCoverInfoSide.jsx
+++ src/components/viewresources/bookCoverInfoSide.jsx
@@ -12,28 +12,28 @@ const BookCoverandInfo = ({
     <div className="book-cover-info-side">
 
         <div className="info-group">
-            <h3>ISBN:</h3>
+            <h3 className= "info-head">ISBN:</h3>
             <h3 className = "info-value">{isbn}</h3>
             
         </div>
         <hr/>
 
         <div className = "info-group">
-            <h3>Publisher:</h3>
+            <h3 className= "info-head">Publisher:</h3>
             <h3 className = "info-value">{publisher}</h3>
             
         </div>
         <hr/>
 
         <div className = "info-group">
-            <h3>Number of copies available:</h3>
+            <h3 className= "info-head">No. of copies available:</h3>
             <h3 className = "info-value">{numOfCopies}</h3>
            
         </div>
         <hr/>
 
         <div className = "info-group">
-            <h3>Subject(s):</h3>
+            <h3 className= "info-head">Subject(s):</h3>
             {subjects.map((item, key) => (
               <h3 className="info-value" key={key}>
                 {item.subject}
@@ -44,7 +44,7 @@ const BookCoverandInfo = ({
         <hr/>
 
         <div className = "info-group">
-            <h3>Physical Description:</h3>
+            <h3 className= "info-head">Physical Description:</h3>
             <p className = "info-value">{physicalDesc}</p>
            
         </div>
