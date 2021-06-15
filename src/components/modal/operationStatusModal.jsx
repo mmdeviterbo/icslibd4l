687856d3e870dd6fdd5e0e22b2896788c81e3609
johnmelr <jrramos7@up.edu.ph>
2021-06-15 20:35:20 +0800

    Fixed typo error when showing delete success message.

--- src/components/modal/operationStatusModal.jsx
+++ src/components/modal/operationStatusModal.jsx
@@ -5,59 +5,61 @@ import Button from "react-bootstrap/Button";
 import { useHistory } from "react-router";
 
 const StatusModal = ({
-  message,
-  name,
-  show,
-  setShow,
-  operation,
-  pathAfter,
-  // item,
-  isSelf,
+    message,
+    name,
+    show,
+    setShow,
+    operation,
+    pathAfter,
+    // item,
+    isSelf,
 }) => {
-  const history = useHistory();
+    const history = useHistory();
 
-  const handleClose = () => {
-    setShow(false);
-    if (pathAfter) {
-      // console.log(pathAfter);
-      window.location = pathAfter;
-    } else {
-      history.goBack();
-    }
-  };
+    const handleClose = () => {
+        setShow(false);
+        if (pathAfter) {
+            // console.log(pathAfter);
+            window.location = pathAfter;
+        } else {
+            history.goBack();
+        }
+    };
 
-  return (
-    <>
-      <Modal
-        show={show}
-        onHide={handleClose}
-        backdrop="static"
-        keyboard={false}
-        centered
-      >
-        <Modal.Body>
-          {message === "success" ? (
-            <Modal.Title>
-              {isSelf ? "Your account" : name} has been{" "}
-              {operation === "edit" || "add" ? `${operation}ed` : "deleted"}{" "}
-              successfully.
-              {isSelf ? " You will be logged out." : ""}
-            </Modal.Title>
-          ) : (
-            <Modal.Title>
-              Failed to {operation} {name}.
-            </Modal.Title>
-          )}
-        </Modal.Body>
+    return (
+        <>
+            <Modal
+                show={show}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+                centered
+            >
+                <Modal.Body>
+                    {message === "success" ? (
+                        <Modal.Title>
+                            {isSelf ? "Your account" : name} has been{" "}
+                            {operation === "edit" || operation === "add"
+                                ? `${operation}ed`
+                                : "deleted"}{" "}
+                            successfully.
+                            {isSelf ? " You will be logged out." : ""}
+                        </Modal.Title>
+                    ) : (
+                        <Modal.Title>
+                            Failed to {operation} {name}.
+                        </Modal.Title>
+                    )}
+                </Modal.Body>
 
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
-  );
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
 };
 
 export default StatusModal;
