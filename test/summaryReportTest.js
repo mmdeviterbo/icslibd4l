fa7e251132998b0b8ce72492ec9cc4bf8e38f652
Bien Custodio <bbcustodio2>
2021-06-16 18:35:42 +0800

    EDIT: edited the test files

--- test/summaryReportTest.js
+++ test/summaryReportTest.js
@@ -17,7 +17,11 @@ describe("Summary Report Router API", () => {
         it("Generate Summary Report", (done) => {
             // eslint-disable-next-line no-unused-expressions
             axios
-                .get(summaryReportRoute + "/report?type=all")
+                .get(summaryReportRoute + "/report?type=all", {
+                    headers: {
+                        Cookie: "token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InB1YmxpYyI6bnVsbCwiSUNTbGlicmFyeSI6ImU5NDU0Y2VhN2UwYTc0NWQ2ZGZkYjUzNzRhMmYxMTE3MDIxZDcwZDExZDJkYmI5NmYxZWJkZThhZDFmMzYxZTRhYjRjNDlhMjllMTFkZmFiMGNlNWIzMjczZGExOTVjODM0ODU5MzI0N2MyNmZhYWM5YmUwNTA3MzhlNDBmOGY3NjdhYjBhOGU1YThjZTIxZjFmZGE5YTBhNWU2ZjA0ZjgxZGFmYjNkNGQ2M2Q2Nzc2OTUzZWE5MDljZGExN2I2NTc1ZTE0YTI4YmVmOTdlYzc1NzFiYzg2YmI4NWU2NTQ0NzA5YmNiZjk1MzFhOGM5ZmY4NTc1ZTkxYWFiNTMwMjVmZGE1NTBiZmNjMmU4NGNkZGQ1OWVmMWVlMTJhNGM1MyJ9LCJpYXQiOjE2MjM4MjExNzgsImV4cCI6MTYyMzkwNzU3OH0.F-LPE0_4Odr-5HfNJ1hM_sFYGcLOwjFgMVlOKur5fPk",
+                    },
+                })
                 .then((res) => {
                     // eslint-disable-next-line no-unused-expressions
                     expect(fs.existsSync("./src/download/Merged.pdf")).to.be
