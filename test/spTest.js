7a71c1635b4b9673143e764dc7c3373a7413a174
karlbrazil <krbrazil@up.edu.ph>
2021-06-16 22:14:34 +0800

    Edit: added test case in spTest.js

--- test/spTest.js
+++ test/spTest.js
@@ -91,6 +91,82 @@ describe("SP Thesis Router API", () => {
         });
     });
 
+    describe("POST /create ", () => {
+        it("check if resource already exists", () => {
+            let spInfo = {
+                "type": "Thesis",
+                "title": "Test Title2",
+                "abstract": "Title Abstract2",
+                "year": 2020,
+                "keywords" : ["keywords11", "keywords12"],
+                "authors": [
+                    {	"fname" : "First2",
+                        "lname" : "Last2"	
+                    }
+                ],
+                "advisers": [
+                    {	"fname" : "First2",
+                        "lname" : "Last2"	
+                    }
+                ],
+                "source_code": "www",
+                "manuscript": "www",
+                "journal": "www",
+                "poster": "www"
+            };
+            axios
+                .post(spRoute + "/create", spInfo, {
+                    withCredentials: true,
+                    headers: {
+                        Cookie: "token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InB1YmxpYyI6bnVsbCwiSUNTbGlicmFyeSI6ImU5NDU0Y2VhN2UwYTc0NWQ2ZGZkYjUzNzRhMmYxMTE3MDIxZDcwZDExZDJkYmI5NmYxZWJkZThhZDFmMzYxZTRhYjRjNDlhMjllMTFkZmFiMGNlNWIzMjczZGExOTVjODM0ODU5MzI0N2MyNmZhYWM5YmUwNTA3MzhlNDBmOGY3NjdhYjBhOGU1YThjZTIxZjFmZGE5YTBhNWU2ZjA0ZjgxZGFmYjNkNGQ2M2Q2Nzc2OTUzZWE5MDljZGExN2I2NTc1ZTE0YTI4YmVmOTdlYzc1NzFiYzg2YmI4NWU2NTQ0NzA5YmNiZjk1MzFhOGM5ZmY4NTc1ZTkxYWFiNTMwMjVmZGE1NTBiZmNjMmU4NGNkZGQ1OWVmMWVlMTJhNGM1MyJ9LCJpYXQiOjE2MjM4MjExNzgsImV4cCI6MTYyMzkwNzU3OH0.F-LPE0_4Odr-5HfNJ1hM_sFYGcLOwjFgMVlOKur5fPk",
+                    },
+                })
+                .catch((err) => {
+                    expect(err).to.be.instanceOf(Error);
+                    expect(err.errorMessage).to.match(/SP already exists./);
+                    assert.fail(null, null, "SP already exists.");
+                });
+        });
+    });
+
+
+
+    describe("POST /create SP ", () => {
+        it("add a SP to the database without 4 file links", (done) => {
+            let spInfo = {
+                "type": "Special Problem",
+                "title": "Test Title1",
+                "abstract": "Title Abstract1",
+                "year": 2020,
+                "keywords" : ["keywords01", "keywords02"],
+                "authors": [
+                    {	"fname" : "First1",
+                        "lname" : "Last1"	
+                    }
+                ],
+                "advisers": [
+                    {	"fname" : "First1",
+                        "lname" : "Last1"	
+                    }
+                ],
+            };
+            axios
+                .post(spRoute + "/create", spInfo, {
+                    headers: {
+                        Cookie: "token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InB1YmxpYyI6bnVsbCwiSUNTbGlicmFyeSI6ImU5NDU0Y2VhN2UwYTc0NWQ2ZGZkYjUzNzRhMmYxMTE3MDIxZDcwZDExZDJkYmI5NmYxZWJkZThhZDFmMzYxZTRhYjRjNDlhMjllMTFkZmFiMGNlNWIzMjczZGExOTVjODM0ODU5MzI0N2MyNmZhYWM5YmUwNTA3MzhlNDBmOGY3NjdhYjBhOGU1YThjZTIxZjFmZGE5YTBhNWU2ZjA0ZjgxZGFmYjNkNGQ2M2Q2Nzc2OTUzZWE5MDljZGExN2I2NTc1ZTE0YTI4YmVmOTdlYzc1NzFiYzg2YmI4NWU2NTQ0NzA5YmNiZjk1MzFhOGM5ZmY4NTc1ZTkxYWFiNTMwMjVmZGE1NTBiZmNjMmU4NGNkZGQ1OWVmMWVlMTJhNGM1MyJ9LCJpYXQiOjE2MjM4MjExNzgsImV4cCI6MTYyMzkwNzU3OH0.F-LPE0_4Odr-5HfNJ1hM_sFYGcLOwjFgMVlOKur5fPk",
+                    },
+                    withCredentials: true,
+                })
+                .then((res) => {
+                    sp_thesis_id = res.data.sp_thesis_id;
+                    expect(res.status).to.equal(200);
+                    done();
+                })
+                .catch((err) => {
+                    done(err);
+                });
+        });
+    });
     /**********************
      DOWNLOAD route
     ***********************/
